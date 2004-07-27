a99677ca6418ccd156f7685ec7e1676c402cf7c6
Jon Trowbridge <trow@src.gnome.org>
2004-07-27 14:25:43 +0000

    Christopher Orr's pdftotext-based PDF Filter.

---
+++ Filters/FilterPdf.cs
@@ -0,0 +1,51 @@
+//
+// FilterPdf.cs: Very simplistic PDF filter
+//
+// Author:
+//   Christopher Orr <dashboard@protactin.co.uk>
+//
+// Copyright 2004 by Christopher Orr
+//
+
+using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace Beagle.Filters {
+
+	public class FilterPdf : Filter {
+
+		public FilterPdf ()
+		{
+			AddSupportedMimeType ("application/pdf");
+		}
+
+		// FIXME: we should have a reasonable failure mode if pdftotext is
+		// not installed.
+
+		protected override void DoPull ()
+		{
+			// get full file path from Filter
+			string path = CurrentFileInfo.Directory +"/"+ CurrentFileInfo.Name;
+			Console.WriteLine ("Converting PDF \"{0}\"", path);			
+
+			// create new external process
+			Process pc = new Process ();
+			pc.StartInfo.FileName = "pdftotext";
+			// FIXME: We probably need to quote special chars in the path
+			pc.StartInfo.Arguments = "-enc UTF-8 \""+ path +"\" -";
+			pc.StartInfo.RedirectStandardInput = false;
+			pc.StartInfo.RedirectStandardOutput = true;
+			pc.StartInfo.UseShellExecute = false;
+			pc.Start ();
+
+			// add pdftotext's output to pool
+			StreamReader pout = pc.StandardOutput;
+			AppendText (pout.ReadToEnd ());
+			pout.Close ();
+			pc.Close ();
+			
+			Finished ();
+		}
+	}
+}
