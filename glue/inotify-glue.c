4593bacc900a4605a4328b2cc29f122c26246514
Robert Love <rml@novell.com>
2004-10-18 21:30:23 +0000

    clean this up; remove some cruft; do error checking on the read (removes a
    2004-10-18  Robert Love  <rml@novell.com>
    
            * glue/inotify-glue.c: clean this up; remove some cruft; do error
              checking on the read (removes a FIXME)
    
    Note: Dave Camp is an assassin.

--- glue/inotify-glue.c
+++ glue/inotify-glue.c
@@ -1,10 +1,7 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-
-
 /*
  * inotify-glue.c
  *
- * Copyright (C) 2004 The Free Software Foundation, Inc.
+ * Copyright (C) 2004 Novell, Inc.
  *
  */
 
@@ -31,8 +28,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -47,9 +42,8 @@ int inotify_glue_open_dev ()
 	int fd;
 
 	fd = open("/dev/inotify", O_RDONLY);
-
 	if (fd < 0)
-		perror ("open(\"/dev/inotify\", O_RDONLY) = ");
+		perror ("open");
 
 	return fd;
 }
@@ -58,12 +52,12 @@ int inotify_glue_close_dev (int fd)
 {
 	int r;
 
-	if ( (r = close (fd)) < 0)
-		perror ("close (fd) = ");
+	r = close (fd);
+	if (r < 0)
+		perror ("close");
 
 	return r;
 }
-
 
 int inotify_glue_watch (int fd, const char *filename, unsigned long mask)
 {
@@ -72,45 +66,43 @@ int inotify_glue_watch (int fd, const char *filename, unsigned long mask)
 	iwr.mask = mask;
 	int wd;
 
-	wd = ioctl(fd, INOTIFY_WATCH, &iwr);
+	wd = ioctl (fd, INOTIFY_WATCH, &iwr);
 	if (wd < 0) {
-		printf("ioctl(fd, INOTIFY_WATCH, {%s, %ld}) =",
-		       iwr.dirname, iwr.mask);
-		fflush(stdout);
-		perror (" ");
+		fprintf (stderr, "ioctl(%d, INOTIFY_WATCH, {%s, %ld}) failed",
+		        fd, iwr.dirname, iwr.mask);
+		perror ("ioctl");
 	}
 
 	free (iwr.dirname);
 
-#ifdef VERBOSE
-	printf("%s WD=%d\n", filename, wd);
-#endif
 	return wd;
 }
 
 int inotify_glue_ignore (int fd, int wd)
 {
-	int r;
+	int ret;
 
-	r = ioctl (fd, INOTIFY_IGNORE, &wd);
-	if (r < 0)
-		perror("ioctl(fd, INOTIFY_IGNORE, &wid) = ");
+	ret = ioctl (fd, INOTIFY_IGNORE, &wd);
+	if (ret < 0) {
+		fprintf (stderr, "ioctl(%d, INOTIFY_IGNORE, %d) failed",
+			 fd, wd);
+		perror ("ioctl");
+	}
 
-	return r;
+	return ret;
 }
 
-int inotify_glue_try_for_event (int fd, int sec, int usec, inotify_event_callback callback)
+int inotify_glue_try_for_event (int fd, int sec, int usec,
+				inotify_event_callback callback)
 {
   	struct timeval timeout;
-	int r;
 	fd_set rfds;
+	struct inotify_event event;
+	char *event_buffer;
+	int num_bytes, remaining, ret;
 
-    struct inotify_event event;
-    char *event_buffer;
-    int num_bytes, remaining;
-
-    if (callback == NULL)
-        return 0;
+	if (!callback)
+        	return 0;
 
 	timeout.tv_sec = sec;
 	timeout.tv_usec = usec;
@@ -118,25 +110,36 @@ int inotify_glue_try_for_event (int fd, int sec, int usec, inotify_event_callbac
 	FD_ZERO (&rfds);
 	FD_SET (fd, &rfds);
 
-	r = select (fd+1, &rfds, NULL, NULL, &timeout);
-    
-    /* We couldn't find an event. */
-    if (r <= 0)
-        return 0;
+	ret = select (fd + 1, &rfds, NULL, NULL, &timeout);
 
-    event.wd = 0;
-    event.mask = 0;
+	/* We couldn't find an event. */
+	if (ret <= 0)
+		return 0;
 
-    /* FIXME: We need to check for errors, etc. */
-    remaining = sizeof (struct inotify_event);
-    event_buffer = (char *) &event;
-    while (remaining > 0) {
-        num_bytes = read (fd, event_buffer, remaining);
-        event_buffer += num_bytes;
-        remaining -= num_bytes;
-    }
+	event.wd = 0;
+	event.mask = 0;
 
-    callback (event.wd, event.mask, event.cookie, event.filename);
+	remaining = sizeof (struct inotify_event);
+	while (remaining > 0) {
+        	num_bytes = read (fd, &event, remaining);
+		/*
+		 * If num_bytes==0, this would be an unexpected EOF, resulting
+		 * in a partial read of the inotify_event structure, so return.
+		 */
+		if (!num_bytes) {
+			fprintf (stderr, "Unexpected EOF on read()\n");
+			return 0;
+		}
+		/* If num_bytes<0, we have an error.  Return there, too. */
+		if (num_bytes < 0) {
+			perror ("read");
+			return 0;
+		}
+		event_buffer += num_bytes;
+		remaining -= num_bytes;
+	}
 
-    return 1;
+	callback (event.wd, event.mask, event.cookie, event.filename);
+
+	return 1;
 }
