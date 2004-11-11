c9f9eb177b003ad17da25159df69474d35360c51
Fredrik Hedberg <fhedberg@src.gnome.org>
2004-11-11 14:15:27 +0000

    Added support for indexing of Monodoc source documentation files. Not enabled in Makefile.am.

---
+++ beagled/MonodocQueryable/MonodocQueryable.cs
@@ -0,0 +1,256 @@
+//
+// MonodocQueryable.cs
+//
+// Copyright (C) 2004 Novell, Inc.
+//
+// Authors:
+//   Fredrik Hedberg (fredrik.hedberg@avafan.com)
+//
+
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Threading;
+
+using Beagle.Daemon;
+using Beagle.Util;
+
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace Beagle.Daemon.MonodocQueryable {
+
+	[QueryableFlavor (Name="Monodoc", Domain=QueryDomain.Local)]
+	public class MonodocQueryable : LuceneQueryable {
+
+		private static Logger log = Logger.Get ("MonodocQueryable");
+
+		string monodocDir;
+
+		public MonodocQueryable () : base (Path.Combine (PathFinder.RootDir, "MondocIndex"))
+		{
+			monodocDir = "/usr/lib/monodoc/sources"; // FIXME Make use of autoconf
+		}
+
+		public override void Start () 
+		{			
+			if (! (Directory.Exists (monodocDir)))
+				return;
+
+			base.Start ();
+
+			Thread th = new Thread (new ThreadStart (StartWorker));
+			th.Start ();
+		}
+		
+		private void StartWorker () 
+		{
+			Inotify.Event += OnInotifyEvent;
+
+			log.Info ("Scanning Monodoc sources");
+			Stopwatch timer = new Stopwatch ();
+			timer.Start ();
+			int foundSources;
+			int foundTypes;
+			Scan (out foundSources, out foundTypes);
+			timer.Stop ();
+			log.Info ("Found {0} types in {1} Monodoc sources in {2}", foundTypes, foundSources, timer);
+		}
+
+		private void Scan (out int foundSources, out int foundTypes)
+		{
+			foundSources = 0;
+			foundTypes = 0;
+
+			DirectoryInfo root = new DirectoryInfo (monodocDir);
+			if (! root.Exists)
+				return;
+
+			int wd = Inotify.Watch (root.FullName, Inotify.EventType.Modify);
+			
+			foreach (FileInfo file in root.GetFiles ("*.zip")) {
+ 				int result = IndexArchive (file, Scheduler.Priority.Delayed);
+				if (result != -1) {
+					foundSources++;
+					foundTypes += result;
+				}
+			}
+		}
+
+
+		private void OnInotifyEvent (int wd,
+					     string path,
+					     string subitem,
+					     Inotify.EventType type,
+					     uint cookie)
+		{
+			if (subitem == "")
+				return;
+
+			string full_path = Path.Combine (path, subitem);
+
+			switch (type) {
+			case Inotify.EventType.Modify:
+				IndexArchive (new FileInfo(full_path), Scheduler.Priority.Immediate);
+				break;
+			}
+		}
+
+		int IndexArchive (FileInfo file, Scheduler.Priority priority)
+		{
+			if (Driver.IsUpToDate (file.FullName))
+				return -1;
+
+			log.Info ("Scanning " + file);
+
+			Scheduler.TaskGroup group = NewMarkingTaskGroup (file.FullName, file.LastWriteTime);
+			
+			int countTypes = 0;			
+			ZipFile archive = new ZipFile (file.ToString());
+			
+			foreach (ZipEntry entry in archive)
+			{
+				if (entry.Name.IndexOf (".") != -1)
+					continue;
+
+				log.Debug ("Going through entry " + entry.Name);
+
+				XmlDocument document = new XmlDocument ();
+				document.Load (archive.GetInputStream (entry));
+			
+				XmlNode type = document.SelectSingleNode ("/Type");
+
+				if (type == null)
+					continue;
+
+				Indexable typeIndexable = TypeNodeToIndexable(type,file);
+				
+				Scheduler.Task typeTask = NewAddTask (typeIndexable);
+				typeTask.Priority = priority;
+				typeTask.SubPriority = 0;
+				typeTask.AddTaskGroup (group);
+				ThisScheduler.Add (typeTask);
+
+				foreach(XmlNode member in type.SelectNodes("Members/Member"))
+				{
+					Indexable memberIndexable = MemberNodeToIndexable(
+						member,
+						file,
+						type.Attributes["FullName"].Value);
+
+					Scheduler.Task memberTask = NewAddTask (memberIndexable);
+					memberTask.Priority = priority;
+					memberTask.SubPriority = 0;
+					memberTask.AddTaskGroup (group);
+					ThisScheduler.Add (memberTask);
+				}
+				countTypes++;
+			}
+
+			return countTypes;
+		}
+
+		Indexable TypeNodeToIndexable(XmlNode node,FileInfo file)
+		{
+			log.Debug ("Parsing T:" + node.Attributes["FullName"].Value);
+
+			Indexable indexable = new Indexable(
+				new Uri ("monodoc:///" + file + ";item=T:"+node.Attributes["FullName"].Value));
+
+			indexable.MimeType = "application/monodoc";
+			indexable.Type = "Monodoc";
+
+			indexable.AddProperty (Property.NewKeyword ("fixme:fullname", node.Attributes["FullName"].Value));
+			indexable.AddProperty (Property.NewKeyword ("fixme:type", "type"));
+
+			// Should we add other stuff here? Implemented interfaces etc?
+
+			StringReader reader = new StringReader (node.SelectSingleNode ("Docs").InnerXml); 
+                        indexable.SetTextReader (reader);
+
+			return indexable;
+		}
+		
+		Indexable MemberNodeToIndexable(XmlNode node, FileInfo file, string parentName)
+		{
+			char memberType = MemberTypeToChar (node.SelectSingleNode ("MemberType").InnerText);
+			StringBuilder memberFullName = new StringBuilder ();
+	
+			memberFullName.Append (memberType + ":"+ parentName);
+
+			if (memberType != 'C')
+				memberFullName.Append ("." + node.Attributes["MemberName"].Value);
+
+			if (memberType == 'C' || memberType == 'M' || memberType == 'E')
+			{	
+				memberFullName.Append ("(");
+				bool inside = false;
+
+				foreach (XmlNode parameter in node.SelectNodes ("Parameters/Parameter"))
+				{	
+					if (!inside) inside = true; else memberFullName.Append(",");
+					memberFullName.Append (parameter.Attributes["Type"].Value);
+				}
+
+				memberFullName.Append (")");
+			}
+
+			log.Debug ("Parsing " + memberFullName);
+
+			Indexable indexable = new Indexable (
+				new Uri ("monodoc://" + file + ";item=" + memberFullName));
+
+			indexable.MimeType = "application/monodoc";
+			indexable.Type = "Monodoc";
+
+			indexable.AddProperty (
+				Property.NewKeyword ("fixme:fullname",memberFullName));
+			indexable.AddProperty (
+				Property.NewKeyword ("fixme:type", node.SelectSingleNode ("MemberType").InnerText.ToLower ()));
+
+			StringReader reader = new StringReader (node.SelectSingleNode ("Docs").InnerXml); 
+                        indexable.SetTextReader (reader);
+
+			return indexable;			
+		}
+
+		char MemberTypeToChar (string memberType)
+		{
+			switch (memberType) 
+			{
+				case "Constructor":
+					return 'C';
+				case "Event":
+					return 'E';
+				case "Property":
+					return 'P';
+				case "Field":
+					return 'F';
+				case "Method":
+					return 'M';
+				default:
+					return 'U';
+			}
+		}
+	}
+}
