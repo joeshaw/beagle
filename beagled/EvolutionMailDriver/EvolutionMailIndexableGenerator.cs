f1d78b679c405552f8076d09939e2b49b4d9ba51
Joe Shaw <joeshaw@src.gnome.org>
2004-12-03 20:27:13 +0000

    Don't add 1 to the offset when saving it, it seems to confuse the gmime parser sometimes

--- beagled/EvolutionMailDriver/EvolutionMailIndexableGenerator.cs
+++ beagled/EvolutionMailDriver/EvolutionMailIndexableGenerator.cs
@@ -307,7 +307,7 @@ namespace Beagle.Daemon {
 				EvolutionMailQueryable.log.Debug ("{0}: Finished indexing {1} messages",
 								  this.folder_name, this.indexed_count);
 
-				this.MboxLastOffset = offset + 1;
+				this.MboxLastOffset = offset;
 				this.CrawlFinished ();
 
 				return false;
@@ -425,7 +425,7 @@ namespace Beagle.Daemon {
 							  this.folder_name, this.indexed_count);
 
 			if (this.mbox_parser != null)
-				this.MboxLastOffset = this.mbox_parser.FromOffset + 1;
+				this.MboxLastOffset = this.mbox_parser.FromOffset;
 		}
 
 		public override string GetTarget ()
