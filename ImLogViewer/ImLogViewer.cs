8277bd1555da7d0bb4752916ed42952365b3e6cd
Lukas Lipka <llipka@src.gnome.org>
2005-01-25 20:13:58 +0000

    IM Viewer - Use Gtk.TextView instead of Gecko to render the conversation (R. Slinckx). Type ahead searching has landed (still a little buggy and slow).

--- ImLogViewer/ImLogViewer.cs
+++ ImLogViewer/ImLogViewer.cs
@@ -2,6 +2,7 @@
 // ImLogViewer.cs
 //
 // Lukas Lipka <lukas@pmad.net>
+// Raphael  Slinckx <rslinckx@gmail.com>
 //
 // Copyright (C) 2005 Novell, Inc.
 //
@@ -19,19 +20,18 @@ using Beagle.Util;
 namespace ImLogViewer {
 
 	public class GaimLogViewer {
-		[Widget] Window window1;
-		[Widget] TreeView logsviewer;
-		[Widget] ScrolledWindow logwindow;
+		[Widget] Window logwindow;
+		[Widget] TreeView timelinetree;
+		[Widget] ScrolledWindow scrollwindow;
 		[Widget] Button   searchbutton;
 		[Widget] Label    title;
 		[Widget] Entry    search;
-		
-		private Timeline timeline;
+		[Widget] TextView conversation;
 		
 		private TreeStore treeStore;
 		private CellRendererText renderer;
 
-		private Gecko.WebControl gecko;
+		private Timeline timeline;
 
 		private string log_path;
 		private string first_selected_log;
@@ -39,7 +39,8 @@ namespace ImLogViewer {
 		private TreeIter first_selection_iter;
 
 		private string speaking_to;
-		
+
+
 		public GaimLogViewer (string path) {
 			
 			if (Directory.Exists (path)) {
@@ -62,22 +63,24 @@ namespace ImLogViewer {
 			if (dt.Ticks > 0)
 				str += ": " + StringFu.DateTimeToPrettyString (dt);
 			str += "</b>";
-			this.title.Markup = str;
+			title.Markup = str;
 		}
 		
 		Gtk.AccelGroup accel_group;
 		GlobalKeybinder global_keys;
 
-		private void ShowWindow (string speaker) {
+		private void ShowWindow (string speaker)
+		{
 			Application.Init();
 			
-			Glade.XML gxml = new Glade.XML (null, "ImLogViewer.glade", "window1", null);
+			Glade.XML gxml = new Glade.XML (null, "ImLogViewer.glade", "logwindow", null);
 			gxml.Autoconnect (this);
 
 			accel_group = new Gtk.AccelGroup ();
-			window1.AddAccelGroup (accel_group);
+			logwindow.AddAccelGroup (accel_group);
 			global_keys = new GlobalKeybinder (accel_group);
 
+			SetTitle (new DateTime ());
 
 			// Close window (Ctrl-W)
 			global_keys.AddAccelerator (new EventHandler (this.HideWindow),
@@ -91,56 +94,80 @@ namespace ImLogViewer {
 						    0,
 						    Gtk.AccelFlags.Visible);
 
-			gecko = new Gecko.WebControl();
-			logwindow.AddWithViewport(gecko);
-			gecko.RenderData("", "file:///tmp", "text/html");
-			gecko.Show();
+			TextTag boldtag = new TextTag ("bold");
+			boldtag.Weight = Pango.Weight.Bold;
+			conversation.Buffer.TagTable.Add (boldtag);
 		
-			this.treeStore = new TreeStore(new Type[] {typeof(string), typeof(string), typeof(object)});
-			this.logsviewer.Model = this.treeStore;
-			
-			this.renderer = new CellRendererText();
+			treeStore = new TreeStore(new Type[] {typeof(string), typeof(string), typeof(object)});
+			timelinetree.Model = this.treeStore;
+			renderer = new CellRendererText();
 			
 			//FIXME: Hide the expanders in the timeline widget
-			/*TreeViewColumn hidden = logsviewer.AppendColumn ("HiddenExpander", renderer , "text", 3);
+			/*TreeViewColumn hidden = timelinetree.AppendColumn ("HiddenExpander", renderer , "text", 3);
 			  hidden.Visible = false;
-			  logsviewer.ExpanderColumn = hidden;*/
+			  timelinetree.ExpanderColumn = hidden;*/
 			
-			logsviewer.AppendColumn ("Date", renderer , "markup", 0);
-			logsviewer.AppendColumn ("Snippet", renderer , "text", 1);
-
-			SetTitle (new DateTime ());
+			timelinetree.AppendColumn ("Date", renderer , "markup", 0);
+			timelinetree.AppendColumn ("Snippet", renderer , "text", 1);
 
 			PopulateTimelineWidget ();
 
-			logsviewer.ExpandAll();
-
-			logsviewer.Selection.Changed += OnConversationSelected; 
-			//search.Activated += OnSearchPressed;
+			timelinetree.Selection.Changed += OnConversationSelected; 
+			search.Activated += OnSearch;
+			search.Changed += OnTypeAhead;
+			searchbutton.Activated += OnSearch;
 
 			if (have_first_selection_iter)
-				logsviewer.Selection.SelectIter (first_selection_iter);
+				timelinetree.Selection.SelectIter (first_selection_iter);
 			
 			Application.Run();
 		}
 		
+		private bool LogContainsString (ImLog log, string text)
+ 		{
+			string [] words = text.Split (null);
+			
+			//FIXME: This is quite crude and EXPENSIVE!
+			foreach (string word in words)	{
+				bool match = false;
+
+				foreach (ImLog.Utterance utt in log.Utterances)	{
+					if (utt.Text.ToLower ().IndexOf (word.ToLower ()) != -1) {
+						match = true;
+						break;
+					}
+				}
+			
+				if (!match) return false;
+			}
+									
+			return true;
+		}
+
+		private void UpdateTimelineTree ()
+		{
+			treeStore = new TreeStore(new Type[] {typeof(string), typeof(string), typeof(object)});
+			timelinetree.Model = this.treeStore;
+			PopulateTimelineWidget ();
+		}
+
 		private void PopulateTimelineWidget ()
 		{
 			TreeIter parent;
 			
 			if (timeline.Today.Count != 0) {
-				parent= treeStore.AppendValues ("<b>Today</b>", "", null);
-				PopulateTimeline(parent, timeline.Today, "HH:mm");
+				parent = treeStore.AppendValues ("<b>Today</b>", "", null);
+				PopulateTimeline (parent, timeline.Today, "HH:mm");
 			}
 			
 			if (timeline.Yesterday.Count != 0) {
 				parent = treeStore.AppendValues ("<b>Yesterday</b>", "", null);
-				PopulateTimeline(parent, timeline.Yesterday, "HH:mm");
+				PopulateTimeline (parent, timeline.Yesterday, "HH:mm");
 			}
 			
 			if (timeline.ThisWeek.Count != 0) {
 				parent = treeStore.AppendValues ("<b>This Week</b>", "", null);
-				PopulateTimeline(parent, timeline.ThisWeek, "dddd");
+				PopulateTimeline (parent, timeline.ThisWeek, "dddd");
 			}
 		
 			if (timeline.LastWeek.Count != 0) {
@@ -150,7 +177,7 @@ namespace ImLogViewer {
 			
 			if (timeline.ThisMonth.Count != 0) {
 				parent = treeStore.AppendValues ("<b>This Month</b>", "", null);
-				PopulateTimeline(parent, timeline.ThisMonth, "MMM d");
+				PopulateTimeline (parent, timeline.ThisMonth, "MMM d");
 			}
 			
 			if (timeline.ThisYear.Count != 0) {
@@ -162,6 +189,8 @@ namespace ImLogViewer {
 				parent = treeStore.AppendValues ("<b>Older</b>", "", null);
 				PopulateTimeline (parent, timeline.Older, "yyyy MMM d");
 			}
+
+			timelinetree.ExpandAll();
 		}
 
 		private class ReverseLogComparer : IComparer {
@@ -178,6 +207,10 @@ namespace ImLogViewer {
 		{
 			list.Sort (rev_cmp);
 			foreach (ImLog log in list) {
+				if (search.Text != null && search.Text != "")
+					if (!LogContainsString (log, search.Text))
+						continue;
+					
 				string date_str = log.StartTime.ToString (dateformat);
 				TreeIter iter;
 				iter = treeStore.AppendValues (parent, date_str, log.EllipsizedSnippet, log);
@@ -205,28 +238,26 @@ namespace ImLogViewer {
 				}
 			}
 		}
-		
-		private void RenderConversation (ImLog im_log)
-		{
-			if (im_log == null) {
-				SetTitle (new DateTime ());
-				gecko.RenderData ("", "file:///tmp/FIXME", "text/html");
-				return;
-			}
-				
-			SetTitle (im_log.StartTime);
 
-			StringBuilder html = new StringBuilder ();
-
-			foreach (ImLog.Utterance utt in im_log.Utterances) {
-				//FIXME: We strip html tags here!
-				string who = utt.Who;
-				who = who.Replace (" ", "&nbsp;");
-				html.Append ("<p><b>" + who + ":</b> " + utt.Text + "</p>\n");
-			}
-				
-			gecko.RenderData(html.ToString (), StringFu.PathToQuotedFileUri (im_log.LogFile), "text/html");
+ 		private void RenderConversation (ImLog im_log)
+ 		{
+			TextBuffer buffer = conversation.Buffer;
+			buffer.Delete (buffer.StartIter, buffer.EndIter);
 			
+ 			if (im_log == null) {
+ 				SetTitle (new DateTime ());
+ 				return;
+ 			}
+ 				
+ 			SetTitle (im_log.StartTime);
+			
+			TextTag bold = buffer.TagTable.Lookup("bold");
+ 			foreach (ImLog.Utterance utt in im_log.Utterances) {
+ 				string who = utt.Who;
+				
+				buffer.InsertWithTags (buffer.EndIter, who + ":", new TextTag[] {bold});
+				buffer.Insert (buffer.EndIter, String.Format(" {0}\n", utt.Text));
+			}
 		}
 
 		private void HideWindow (object o, EventArgs args)
@@ -238,6 +269,15 @@ namespace ImLogViewer {
 		{
 			Application.Quit ();
 			args.RetVal = true;
+		}
+
+		private void OnSearch (object o, EventArgs args)
+		{
+		}
+
+		private void OnTypeAhead (object o, EventArgs args)
+		{
+			UpdateTimelineTree ();
 		}
 		
 		private void OnConversationSelected (object o, EventArgs args) 
