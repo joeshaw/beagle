c370f1be87cbafb6d554291cbbc0b97656224fa5
Robert Love <rml@src.gnome.org>
2005-02-01 19:10:21 +0000

    There are currently 5,381 coding styles in glue/inotify-glue.c.  Unify them down to only one with my amazing cool gindent script and make Chris Rivera shut the hell up.  Seriously.  Does this kid have nothing better to do than complain about coding style in stuff I work on?  And--one other thing--why does he smell like cabbage?

--- glue/inotify-glue.c
+++ glue/inotify-glue.c
@@ -42,45 +42,45 @@
 #define SYSFS_MAX_USER_WATCHES  SYSFS_PREFIX "/max_user_watches"
 #define SYSFS_MAX_QUEUED_EVENTS SYSFS_PREFIX "/max_queued_events"
 
-static int max_user_devices  = 8;     /* This is the pre-sysfs default */
-static int max_user_watches  = 8192;  /* This is the pre-sysfs default */
-static int max_queued_events = 256;   /* This is the pre-sysfs default */
+static int max_user_devices = 8;	/* This is the pre-sysfs default */
+static int max_user_watches = 8192;	/* This is the pre-sysfs default */
+static int max_queued_events = 256;	/* This is the pre-sysfs default */
 
 /* Paranoid code to read an integer from a sysfs (well, any) file. */
 static void
 read_int (const char *filename, int *var)
 {
-    int fd, n;
-    char buffer[32];
-    char *buffer_endptr = NULL;
+	int fd, n;
+	char buffer[32];
+	char *buffer_endptr = NULL;
 
-    fd = open (filename, O_RDONLY);
-    if (fd == -1)
-	return;
-    if (read (fd, buffer, 31) > 0) {
-	n = (int) strtol (buffer, &buffer_endptr, 10);
-	if (*buffer != '\0' && *buffer_endptr == '\0') 
-	    *var = n;
-    }
-    close (fd);
+	fd = open (filename, O_RDONLY);
+	if (fd == -1)
+		return;
+	if (read (fd, buffer, 31) > 0) {
+		n = (int) strtol (buffer, &buffer_endptr, 10);
+		if (*buffer != '\0' && *buffer_endptr == '\0')
+			*var = n;
+	}
+	close (fd);
 }
 
 
 void
 inotify_glue_init (void)
 {
-    static int initialized = 0;
-    if (initialized)
-	return;
-    initialized = 1;
+	static int initialized = 0;
+	if (initialized)
+		return;
+	initialized = 1;
 
-    read_int (SYSFS_MAX_USER_DEVICES,  &max_user_devices);
-    read_int (SYSFS_MAX_USER_WATCHES,  &max_user_watches);
-    read_int (SYSFS_MAX_QUEUED_EVENTS, &max_queued_events);
+	read_int (SYSFS_MAX_USER_DEVICES, &max_user_devices);
+	read_int (SYSFS_MAX_USER_WATCHES, &max_user_watches);
+	read_int (SYSFS_MAX_QUEUED_EVENTS, &max_queued_events);
 }
 
 
-int 
+int
 inotify_glue_watch (int fd, const char *filename, __u32 mask)
 {
 	struct inotify_watch_request iwr;
@@ -95,8 +95,9 @@ inotify_glue_watch (int fd, const char *filename, __u32 mask)
 
 	wd = ioctl (fd, INOTIFY_WATCH, &iwr);
 	if (wd < 0) {
-		fprintf (stderr, "ioctl(%d, INOTIFY_WATCH, {%s, %d}) failed",
-		        fd, iwr.dirname, iwr.mask);
+		fprintf (stderr,
+			 "ioctl(%d, INOTIFY_WATCH, {%s, %d}) failed", fd,
+			 iwr.dirname, iwr.mask);
 		perror ("ioctl");
 	}
 
@@ -106,7 +107,7 @@ inotify_glue_watch (int fd, const char *filename, __u32 mask)
 }
 
 
-int 
+int
 inotify_glue_ignore (int fd, int wd)
 {
 	int ret;
@@ -129,73 +130,72 @@ inotify_glue_ignore (int fd, int wd)
 void
 inotify_snarf_events (int fd, int timeout_secs, int *nr, void **buffer_out)
 {
-    struct timeval timeout;
-    fd_set read_fds;
-    int select_retval;
-    int pending, prev_pending, pending_pause_count;
-    static struct inotify_event *buffer = NULL;
-    static size_t buffer_size;
+	struct timeval timeout;
+	fd_set read_fds;
+	int select_retval;
+	int pending, prev_pending, pending_pause_count;
+	static struct inotify_event *buffer = NULL;
+	static size_t buffer_size;
 
-    /* Allocate our buffer the first time we try to read events. */
-    if (buffer == NULL) {
-	buffer_size = sizeof (struct inotify_event) + 16;  /* guess avg len */
-	buffer_size *= max_queued_events;
-	buffer = malloc (buffer_size);
-	if (!buffer) {
-		/* FIXME: We should have some method of actual error here .. */
-		perror ("malloc");
-		*buffer_out = NULL;
-		return;
+	/* Allocate our buffer the first time we try to read events. */
+	if (buffer == NULL) {
+		/* guess the avg len */
+		buffer_size = sizeof (struct inotify_event) + 16;
+		buffer_size *= max_queued_events;
+		buffer = malloc (buffer_size);
+		if (!buffer) {
+			perror ("malloc");
+			*buffer_out = NULL;
+			return;
+		}
 	}
-    }
 
-    /* Wait for the file descriptor to be ready to read. */
+	/* Wait for the file descriptor to be ready to read. */
 
-    timeout.tv_sec = timeout_secs;
-    timeout.tv_usec = 0;
+	timeout.tv_sec = timeout_secs;
+	timeout.tv_usec = 0;
 
-    FD_ZERO (&read_fds);
-    FD_SET (fd, &read_fds);
+	FD_ZERO (&read_fds);
+	FD_SET (fd, &read_fds);
 
-    select_retval = select (fd+1, &read_fds, NULL, NULL, &timeout);
+	select_retval = select (fd + 1, &read_fds, NULL, NULL, &timeout);
 
-    /* If we time out, just return */
-    if (select_retval == 0)
-	    return;
+	/* If we time out, just return */
+	if (select_retval == 0)
+		return;
 
-    /* Reading events in groups significantly helps performance.
-       If there are some events (but not too many!) ready, wait a
-       bit more to see if more events come in. */
+	/* Reading events in groups significantly helps performance.
+	 * If there are some events (but not too many!) ready, wait a
+	 * bit more to see if more events come in. */
 
-    prev_pending = 0;
-    pending_pause_count = 0;
+	prev_pending = 0;
+	pending_pause_count = 0;
 
-    while (pending_pause_count < MAX_PENDING_PAUSE_COUNT) {
+	while (pending_pause_count < MAX_PENDING_PAUSE_COUNT) {
 
-	if (ioctl (fd, FIONREAD, &pending) == -1)
-	    break;
-	pending /= sizeof (struct inotify_event) + 16;	/* guess avg len */
+		if (ioctl (fd, FIONREAD, &pending) == -1)
+			break;
+		pending /= sizeof (struct inotify_event) + 16;	/* guess len */
 
-	/* Don't wait if the number of pending events is too close
-	   to the maximum queue size. */
-	if (pending > PENDING_THRESHOLD (max_queued_events))
-	    break;
+		/* Don't wait if the number of pending events is too close
+		 * to the maximum queue size. */
+		if (pending > PENDING_THRESHOLD (max_queued_events))
+			break;
 
-	/* With each successive iteration, the minimum rate for
-	   further sleep doubles. */
-	if (pending - prev_pending < (1 << pending_pause_count))
-	    break;
+		/* With each successive iteration, the minimum rate for
+		 * further sleep doubles. */
+		if (pending - prev_pending < (1 << pending_pause_count))
+			break;
 
-	prev_pending = pending;
-	++pending_pause_count;
+		prev_pending = pending;
+		++pending_pause_count;
 
-	timeout.tv_sec = 0;
-	timeout.tv_usec = PENDING_PAUSE_MICROSECONDS;
-	select (0, NULL, NULL, NULL, &timeout);
-    }
+		timeout.tv_sec = 0;
+		timeout.tv_usec = PENDING_PAUSE_MICROSECONDS;
+		select (0, NULL, NULL, NULL, &timeout);
+	}
 
-    *nr = read (fd, buffer, buffer_size);
+	*nr = read (fd, buffer, buffer_size);
 
-    *buffer_out = buffer;
+	*buffer_out = buffer;
 }
-
