a98da3b833967abc2d522f674dfaedca74036676
Fredrik Hedberg <fhedberg@src.gnome.org>
2005-04-05 19:54:17 +0000

    Hack to make the template system intltool compatible. When you add strings to the templates be sure to run Tiles/extract-translations.pl afterwards.

---
+++ Tiles/TranslationHack.cs
@@ -0,0 +1,96 @@
+using Mono.Posix;
+
+public class TranslationHack {
+	private void NotToBecalled () {
+		/* For translators: From template Tiles/template-docs.html */
+		Catalog.GetString ("in folder");
+		/* For translators: From template Tiles/template-docs.html */
+		Catalog.GetString ("Last modified");
+		/* For translators: From template Tiles/template-docs.html */
+		Catalog.GetString ("Title:");
+		/* For translators: From template Tiles/template-docs.html */
+		Catalog.GetString ("Author:");
+		/* For translators: From template Tiles/template-docs.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-docs.html */
+		Catalog.GetString ("Send to...");
+		/* For translators: From template Tiles/template-docs.html */
+		Catalog.GetString ("Reveal in file manager");
+		/* For translators: From template Tiles/template-launcher.html */
+		Catalog.GetString ("Application:");
+		/* For translators: From template Tiles/template-launcher.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-folder.html */
+		Catalog.GetString ("in folder");
+		/* For translators: From template Tiles/template-folder.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-file.html */
+		Catalog.GetString ("in folder");
+		/* For translators: From template Tiles/template-file.html */
+		Catalog.GetString ("Last modified");
+		/* For translators: From template Tiles/template-file.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-file.html */
+		Catalog.GetString ("Send to...");
+		/* For translators: From template Tiles/template-file.html */
+		Catalog.GetString ("Reveal in file manager");
+		/* For translators: From template Tiles/template-im-log.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-mail-message.html */
+		Catalog.GetString ("Folder:");
+		/* For translators: From template Tiles/template-mail-message.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-mail-message.html */
+		Catalog.GetString ("Send IM");
+		/* For translators: From template Tiles/template-music.html */
+		Catalog.GetString ("Track");
+		/* For translators: From template Tiles/template-music.html */
+		Catalog.GetString ("from");
+		/* For translators: From template Tiles/template-music.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-music.html */
+		Catalog.GetString ("Enqueue");
+		/* For translators: From template Tiles/template-music.html */
+		Catalog.GetString ("Reveal in file manager");
+		/* For translators: From template Tiles/template-note.html */
+		Catalog.GetString ("last modified");
+		/* For translators: From template Tiles/template-note.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-picture.html */
+		Catalog.GetString ("in folder");
+		/* For translators: From template Tiles/template-picture.html */
+		Catalog.GetString ("Modified:");
+		/* For translators: From template Tiles/template-picture.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-picture.html */
+		Catalog.GetString ("Send to...");
+		/* For translators: From template Tiles/template-picture.html */
+		Catalog.GetString ("Reveal in file manager");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("in folder");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("Last modified");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("Title:");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("Author:");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("Slides:");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("Send to...");
+		/* For translators: From template Tiles/template-presentation.html */
+		Catalog.GetString ("Reveal in file manager");
+		/* For translators: From template Tiles/template-web-history.html */
+		Catalog.GetString ("Last viewed:");
+		/* For translators: From template Tiles/template-web-history.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-blog.html */
+		Catalog.GetString ("Weblog:");
+		/* For translators: From template Tiles/template-blog.html */
+		Catalog.GetString ("Published:");
+		/* For translators: From template Tiles/template-blog.html */
+		Catalog.GetString ("Open");
+	}
+}
---
+++ Tiles/extract-translations.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+opendir(DIR, ".");
+@files = grep(/\.html$/,readdir(DIR));
+closedir(DIR);
+
+open (OUTPUT, ">TranslationHack.cs");
+
+print OUTPUT "using Mono.Posix;\n\npublic class TranslationHack {\n\tprivate void NotToBecalled () {\n";
+
+foreach $file (@files) {
+    open (TEMPLATE, $file);
+    while ($data = <TEMPLATE>) {
+	@matches = ($data =~ m/\@text\%([^@]*)\@/g);
+	
+	for ($i = 0; $i < scalar (@matches); $i++) {
+	    print OUTPUT "\t\t/* For translators: From template Tiles/$file */\n";
+	    print OUTPUT "\t\tCatalog.GetString (\"$matches[$i]\");\n";
+	}
+    }
+}
+
+print OUTPUT "\t}\n}\n";
+close (OUTPUT)
+
+
+
