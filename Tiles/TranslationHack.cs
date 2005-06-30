846e8e3c091ff1e3971703c6fafedb838d4370a4
Lukas Lipka <llipka@src.gnome.org>
2005-06-30 12:38:24 +0000

    I18N stuff.

--- Tiles/TranslationHack.cs
+++ Tiles/TranslationHack.cs
@@ -2,6 +2,14 @@ using Mono.Posix;
 
 public class TranslationHack {
 	private void NotToBecalled () {
+		/* For translators: From template Tiles/template-blog.html */
+		Catalog.GetString ("Weblog:");
+		/* For translators: From template Tiles/template-blog.html */
+		Catalog.GetString ("by");
+		/* For translators: From template Tiles/template-blog.html */
+		Catalog.GetString ("Published:");
+		/* For translators: From template Tiles/template-blog.html */
+		Catalog.GetString ("Open");
 		/* For translators: From template Tiles/template-docs.html */
 		Catalog.GetString ("in folder");
 		/* For translators: From template Tiles/template-docs.html */
@@ -16,14 +24,6 @@ public class TranslationHack {
 		Catalog.GetString ("Send to...");
 		/* For translators: From template Tiles/template-docs.html */
 		Catalog.GetString ("Reveal in file manager");
-		/* For translators: From template Tiles/template-launcher.html */
-		Catalog.GetString ("Application:");
-		/* For translators: From template Tiles/template-launcher.html */
-		Catalog.GetString ("Open");
-		/* For translators: From template Tiles/template-folder.html */
-		Catalog.GetString ("in folder");
-		/* For translators: From template Tiles/template-folder.html */
-		Catalog.GetString ("Open");
 		/* For translators: From template Tiles/template-file.html */
 		Catalog.GetString ("in folder");
 		/* For translators: From template Tiles/template-file.html */
@@ -34,7 +34,13 @@ public class TranslationHack {
 		Catalog.GetString ("Send to...");
 		/* For translators: From template Tiles/template-file.html */
 		Catalog.GetString ("Reveal in file manager");
-		/* For translators: From template Tiles/template-im-log.html */
+		/* For translators: From template Tiles/template-folder.html */
+		Catalog.GetString ("in folder");
+		/* For translators: From template Tiles/template-folder.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-launcher.html */
+		Catalog.GetString ("Application:");
+		/* For translators: From template Tiles/template-launcher.html */
 		Catalog.GetString ("Open");
 		/* For translators: From template Tiles/template-mail-message.html */
 		Catalog.GetString ("Folder:");
@@ -61,6 +67,10 @@ public class TranslationHack {
 		/* For translators: From template Tiles/template-picture.html */
 		Catalog.GetString ("Modified:");
 		/* For translators: From template Tiles/template-picture.html */
+		Catalog.GetString ("Tags:");
+		/* For translators: From template Tiles/template-picture.html */
+		Catalog.GetString ("Description:");
+		/* For translators: From template Tiles/template-picture.html */
 		Catalog.GetString ("Open");
 		/* For translators: From template Tiles/template-picture.html */
 		Catalog.GetString ("Send to...");
@@ -82,15 +92,31 @@ public class TranslationHack {
 		Catalog.GetString ("Send to...");
 		/* For translators: From template Tiles/template-presentation.html */
 		Catalog.GetString ("Reveal in file manager");
+		/* For translators: From template Tiles/template-spreadsheet.html */
+		Catalog.GetString ("in folder");
+		/* For translators: From template Tiles/template-spreadsheet.html */
+		Catalog.GetString ("Last modified");
+		/* For translators: From template Tiles/template-spreadsheet.html */
+		Catalog.GetString ("Title:");
+		/* For translators: From template Tiles/template-spreadsheet.html */
+		Catalog.GetString ("Author:");
+		/* For translators: From template Tiles/template-spreadsheet.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-spreadsheet.html */
+		Catalog.GetString ("Send to...");
+		/* For translators: From template Tiles/template-spreadsheet.html */
+		Catalog.GetString ("Reveal in file manager");
 		/* For translators: From template Tiles/template-web-history.html */
 		Catalog.GetString ("Last viewed:");
 		/* For translators: From template Tiles/template-web-history.html */
 		Catalog.GetString ("Open");
-		/* For translators: From template Tiles/template-blog.html */
-		Catalog.GetString ("Weblog:");
-		/* For translators: From template Tiles/template-blog.html */
-		Catalog.GetString ("Published:");
-		/* For translators: From template Tiles/template-blog.html */
+		/* For translators: From template Tiles/template-network.html */
+		Catalog.GetString ("from Networked Beagle on host");
+		/* For translators: From template Tiles/template-network.html */
+		Catalog.GetString ("Open");
+		/* For translators: From template Tiles/template-network.html */
+		Catalog.GetString ("Search Networked Beagle");
+		/* For translators: From template Tiles/template-im-log.html */
 		Catalog.GetString ("Open");
 	}
 }
