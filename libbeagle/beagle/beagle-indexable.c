34137ad1731ec53605a65d9155b7d0825db1676f
Joe Shaw <joeshaw@src.gnome.org>
2005-07-06 16:29:18 +0000

    add copyright headers to all the source files

--- libbeagle/beagle/beagle-indexable.c
+++ libbeagle/beagle/beagle-indexable.c
@@ -1,3 +1,30 @@
+/*
+ * beagle-indexable.c
+ *
+ * Copyright (C) 2005 Novell, Inc.
+ *
+ */
+
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #include "beagle-indexable.h"
 #include "beagle-private.h"
 #include "beagle-timestamp.h"
@@ -66,8 +93,8 @@ beagle_indexable_free (BeagleIndexable *indexable)
 		beagle_timestamp_free (indexable->timestamp);
 
 	g_free (indexable->uri);
+	g_free (indexable->content_uri);
 	g_free (indexable->hot_content_uri);
-	g_free (indexable->uri);
 	
 	g_free (indexable->type);
 	g_free (indexable->mime_type);
