815677df0a88ed54cff7dae6a26e752e2931d7cc
Joe Shaw <joeshaw@src.gnome.org>
2005-11-04 17:38:35 +0000

    Exclude include, main, and NULL from the C filter, since while those aren't keywords, they're extremely common in C and probably shouldn't be indexed

--- Filters/FilterC.cs
+++ Filters/FilterC.cs
@@ -36,11 +36,12 @@ namespace Beagle.Filters {
 	public class FilterC : FilterSource {
 
 		static string  [] strKeyWords  = {"auto", "break", "case", "char", "const", 
-						"continue", "default", "do", "double", "else",
-						"enum", "extern", "float", "for", "goto",
-						"if", "int", "long", "register", "return", "short",
-						"signed", "sizeof", "static", "struct", "switch", "typedef",
-						"union", "unsigned", "void", "volatile", "while" };
+						  "continue", "default", "do", "double", "else",
+						  "enum", "extern", "float", "for", "goto",
+						  "if", "include", "int", "long", "main",
+						  "NULL", "register", "return", "short", "signed",
+						  "sizeof", "static", "struct", "switch", "typedef",
+						  "union", "unsigned", "void", "volatile", "while" };
 			
 		public FilterC ()
 		{
