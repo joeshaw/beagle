62e412f32300e1c49786aa363d8fc2e9bab06fc2
Debajyoti Bera <dbera@src.gnome.org>
2006-11-30 17:10:50 +0000

    Fix crash (#380544) and code cleanup in SVG Filter (patch from Alexander Macdonald).

--- Filters/FilterSvg.cs
+++ Filters/FilterSvg.cs
@@ -36,19 +36,11 @@ namespace Beagle.Filters {
 	public class FilterSvg : Beagle.Daemon.Filter {
 		private StringBuilder sb = new StringBuilder ();
 
-		private enum RdfGrabModes {
-			TitleMode,
-			DescriptionMode,
-			DateMode,
-			Num
-		};
-		
-		static private string [] rdf_grab_strings = { "title", "description", "date" };
-		static private string [] rdf_nongrab_strings = { "creator", "contributor", "publisher", "rights" };
-
 		// List of keys that should be ignored when adding to content.
 		// For example, dc:format is the mime type, so it's not interesting text.
-		static private string [] ignore_strings = { "format" };
+		static private string [] ignore_strings = { "format"};
+		
+		static private string dcnamespace = "http://purl.org/dc/elements/1.1/";
 		
 		public FilterSvg ()
 		{
@@ -60,38 +52,41 @@ namespace Beagle.Filters {
 		{
 			XmlTextReader reader = new XmlTextReader (Stream);
 			reader.XmlResolver = null;
-			
-			int depth = 0;
-			bool grab_text = false, ignore_text = false;;
-			string text = "";
-			
+
+			string current_tag_name = "";
+			bool grab_property = false;
+			bool grab_text = false;
+
 			try {
 				while (reader.Read ()) {
 					switch (reader.NodeType) {
 					case XmlNodeType.Element:
 						if (grab_text)
 							break;
+						if (reader.IsEmptyElement)
+							break;
 						
 						if (ArrayFu.IndexOfString (ignore_strings, reader.LocalName) != -1)
-							ignore_text = true;
-						else if (reader.LocalName == "title" || reader.LocalName == "desc") {
+							break;
+						else if (reader.LocalName == "title") {
+							grab_property = true;
+							current_tag_name = "title";
+						} else if (reader.LocalName == "desc") {
+							grab_property = true;
+							current_tag_name = "description";
+						} else if (reader.LocalName == "text") {
 							grab_text = true;
-							depth = reader.Depth;
-						} else if (reader.LocalName == "RDF")
+							current_tag_name = "text";
+						} else if (reader.LocalName == "RDF") {
 							PullRdfProperties (reader, reader.Depth);
-
+						}
 						break;
 
 					case XmlNodeType.Text:
-						text = reader.Value.Trim ();
-						if (text.Length == 0 || ignore_text)
-							break;
-						
-						if (grab_text) {
-							sb.Append (text);
-						} else {
-							AppendText (text);
-							AppendStructuralBreak ();
+						if (grab_property)
+							AddProperty (Property.New ("dc:" + current_tag_name , reader.Value));
+						else if (grab_text) {
+							sb.Append (reader.Value);
 						}
 						break;
 
@@ -101,110 +96,73 @@ namespace Beagle.Filters {
 						break;
 
 					case XmlNodeType.EndElement:
-						if (! (grab_text && depth == reader.Depth))
+						if (reader.LocalName != current_tag_name)
 							break;
-						
-						grab_text = false;
-						ignore_text = false;
 
-						if (reader.LocalName == "title") {
-							AddProperty (Property.New ("dc:title", sb.ToString ()));
-							sb.Length = 0;
-						} else if (reader.LocalName == "desc") {
-							AddProperty (Property.New ("dc:description", sb.ToString ()));
-							sb.Length = 0;
+						if(grab_text) {
+							AppendText (sb.ToString());
+							AppendStructuralBreak ();
 						}
+
+						grab_text = grab_property = false;
 						break;
 					}
 				}
-				
+
 				Finished ();
 			} catch (System.Xml.XmlException e) {
-				Logger.Log.Error ("error parsing xml file {0}", FileInfo.FullName);
+				Logger.Log.Error ("Error parsing xml file {0}", FileInfo.FullName);
 				Logger.Log.Debug (e);
 				Error ();
 			}
 		}
-		
+
 		protected void PullRdfProperties (XmlTextReader reader, int depth)
 		{
-			int grab_mode = -1, nongrab_mode = -1;
-			bool grab_text = false, ignore_text = false, agent_mode = false;
+			string current_tag_name = "";
+			bool grab_text = false;
+			bool grab_date = false;
 
-			string text = "";
-			
 			try {
 				while (reader.Read ()) {
 					if (depth == reader.Depth)
 						return;			
-		
+
 					switch (reader.NodeType) {
 					case XmlNodeType.Element:
 						if (grab_text)
 							break;
+						
+						if (reader.IsEmptyElement)
+							break;
 
 						if (ArrayFu.IndexOfString (ignore_strings, reader.LocalName) != -1)
-							ignore_text = true;
-						else if (reader.LocalName == "Agent")
-							grab_text = agent_mode = true;
-						else {
-							for (int i = 0; i < (int) RdfGrabModes.Num; i++) {
-								if (reader.LocalName == rdf_grab_strings [i]) {
-									grab_text = true;
-									grab_mode = i;
-									break;
-								}
-							}
-
-							for (int i = 0; i < (int) RdfGrabModes.Num; i++) {
-								if (reader.LocalName == rdf_nongrab_strings [i]) {
-									grab_text = false;
-									nongrab_mode = i;
-									break;
-								}
-							}
+							break;
+						else if (reader.NamespaceURI == dcnamespace) {
+							if (reader.LocalName == "date")
+								grab_date = true;
+							else
+								grab_text = true;
 						}
+
+						current_tag_name = reader.LocalName;
 						break;
 
 					case XmlNodeType.Text:
-						text = reader.Value.Trim ();
-						if (text.Length == 0 || ignore_text)
-							break;
-						
-						if (grab_text) {
-							sb.Append(text);
-						} else {
-							AppendText (text);
-							AppendStructuralBreak ();
+						if (grab_text)
+							AddProperty (Property.New ("dc:" + current_tag_name, reader.Value));
+						else if (grab_date) {
+							try {
+								AddProperty (Property.NewDate ("dc:date", System.Convert.ToDateTime (reader.Value.Trim())));
+							} catch (FormatException) {
+								AddProperty (Property.New ("dc:date", reader.Value));
+							}
 						}
 						break;
 
 					case XmlNodeType.EndElement:
-						ignore_text = false;
-
-						if (agent_mode) {
-							if (reader.LocalName ==  "Agent") {
-								agent_mode = grab_text = false;
-								AddProperty (Property.New ("dc:" + rdf_nongrab_strings [nongrab_mode], sb.ToString ()));
-								sb.Length = 0;
-							}
-						} else if (grab_mode >= 0 && reader.LocalName == rdf_grab_strings [grab_mode]) {
-							if (grab_mode == (int) RdfGrabModes.DateMode) {
-								try {
-									AddProperty (Property.NewDate ("dc:date", System.Convert.ToDateTime (sb.ToString ())));
-								} catch (FormatException) {
-									AddProperty (Property.New ("dc:date", sb.ToString ()));
-								}
-							} else {
-								AddProperty (Property.New ("dc:" + rdf_grab_strings [grab_mode], sb.ToString ()));
-							}
-							sb.Length = 0;
-							grab_mode = -1;
-							grab_text = false;
-						} else if (nongrab_mode >= 0 && reader.LocalName == rdf_nongrab_strings [nongrab_mode]) {
-							nongrab_mode = -1;
-							grab_text = false;
-						}
+						if(reader.LocalName == current_tag_name)
+							grab_text = grab_date = false;
 						break;
 					}
 				}
