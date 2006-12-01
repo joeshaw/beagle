b6bf4c8d535c96729aab8bf51b0287bab7bf8775
Joe Shaw <joeshaw@src.gnome.org>
2006-12-01 18:38:55 +0000

    Clean the archive filter up a lot, add support for non-tarred gzip and bzip files

--- Filters/FilterArchive.cs
+++ Filters/FilterArchive.cs
@@ -42,7 +42,11 @@ namespace Beagle.Filters {
 
 	public class FilterArchive : Beagle.Daemon.Filter {
 
-		Archive archive = null;
+		internal delegate ArchiveEntry GetNextEntry ();
+
+		private FileInfo file_info;
+		private Stream archive_stream;
+		private GetNextEntry get_next_entry;
 
 		public FilterArchive ()
 		{
@@ -51,15 +55,52 @@ namespace Beagle.Filters {
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-compressed-tar"));
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-tar"));
 			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-tgz"));
-			//AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-gzip"));
-			//AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-bzip"));
+			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-gzip"));
+			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/x-bzip"));
 		}
 		
-		protected override void DoOpen (FileInfo info)
+		protected override void DoOpen (FileInfo file_info)
 		{
-			archive = new Archive (info.FullName, MimeType);
+			this.file_info = file_info;
+
+			switch (MimeType) {
+			case "application/zip":
+				archive_stream = new ZipInputStream (Stream);
+				get_next_entry = GetNextEntryZip;
+				break;
+
+			case "application/x-bzip-compressed-tar":
+				archive_stream = new TarInputStream (new BZip2InputStream (Stream));
+				get_next_entry = GetNextEntryTar;
+				break;
+
+			case "application/x-compressed-tar":
+			case "application/x-tgz":
+				archive_stream = new TarInputStream (new GZipInputStream (Stream));
+				get_next_entry = GetNextEntryTar;
+				break;
+
+			case "application/x-tar":
+				archive_stream = new TarInputStream (Stream);
+				get_next_entry = GetNextEntryTar;
+				break;
+
+			case "application/x-gzip":
+				archive_stream = new GZipInputStream (Stream);
+				get_next_entry = GetNextEntrySingle;
+				break;
+
+			case "application/x-bzip":
+				archive_stream = new BZip2InputStream (Stream);
+				get_next_entry = GetNextEntrySingle;
+				break;
+
+			default:
+				throw new ArgumentException ("Invalid or unsupported mime type.");
+			}
+
 		}
-		
+				
 		protected override void DoPullProperties ()
 		{
 			// FIXME: Fetch the archive properties.
@@ -69,349 +110,132 @@ namespace Beagle.Filters {
 		{
 			ArchiveEntry a_entry;
 
-			while ((a_entry = archive.GetNextEntry ()) != null) {
+			while ((a_entry = this.get_next_entry ()) != null) {
 				// FIXME: For nested archives, create uid:foo#bar
 				// instead of uid:foo#xxx#bar (avoid duplicates ?)
-				Indexable child = new Indexable (new Uri (Uri.ToString () + "#" + a_entry.Uri, true));
+				Indexable child = new Indexable (new Uri (Uri.ToString () + "#" + a_entry.Name, true));
 
 				child.CacheContent = false;
 				child.MimeType = a_entry.MimeType;
-				child.ContentUri = UriFu.PathToFileUri (a_entry.TempFileName);
+
+				child.ContentUri = UriFu.PathToFileUri (a_entry.TempFile);
 				child.DeleteContent = true;
 
-				//Log.Debug ("Creating child with uri={0}, content-uri={1}",
-				//	a_entry.Uri,
-				//	child.ContentUri);
-				//Console.WriteLine ("Name: {0}, MimeType: {1}", sub_uri, child.MimeType);
+				child.AddProperty (Property.NewKeyword ("fixme:relativeuri", a_entry.Name));
 
 				if (a_entry.Comment != null)
 					child.AddProperty (Property.New ("fixme:comment", a_entry.Comment));
-				child.AddProperty (Property.New ("fixme:name", a_entry.Name));
-				child.AddProperty (Property.NewKeyword ("fixme:size", a_entry.Size));
-				child.AddProperty (Property.New ("fixme:relativeuri", a_entry.Uri));
+
 				foreach (Property prop in Property.StandardFileProperties (Path.GetFileName (a_entry.Name), false))
 					child.AddProperty (prop);
-
-				//child.SetBinaryStream (File.OpenRead (a_entry.Name));
 
 				AddChildIndexable (child);
 			}
 		}
-		
-		protected override void DoClose () 
-		{
-			if (this.archive != null)
-				archive.Close ();
+
+		internal class ArchiveEntry {
+			public string Name;
+			public string MimeType;
+			public DateTime Modified;
+			public string Comment;
+
+			public string TempFile;
 		}
 
-		private class ArchiveEntry {
-			private string name;
-			private long size;
-			private string mimetype;
-			private bool is_directory;
-			private DateTime modified;
-			private string uri;
-			private string comment;
-			private string temp_file_name;
-		
-			public ArchiveEntry ()
-			{
-				name = null;
-				size = 0;
-				mimetype = null;
-				is_directory = false;
-				modified = DateTimeUtil.UnixToDateTimeUtc (0);
-				comment = null;
-				temp_file_name = null;
-			}
+		private static string StoreStreamInTempFile (Stream stream, DateTime mtime)
+		{
+			if (stream == null)
+				return null;
 
-			public string Name {
-				get {
-					return name;
-				}
-
-				set {
-					name = value;
-				}
-			}
-
-			public string MimeType {
-				get {
-					return mimetype;
-				}
-
-				set {
-					mimetype = value;
-				}
-			}
-
-			public System.DateTime Modified {
-				get {
-					return modified;
-				}
-
-				set {
-					modified = value;
-				}
-			}
-
-			public long Size {
-				get {
-					return size;
-				}
-
-				set {
-					size = value;
-				}
-			}
-
-			public bool IsDirectory {
-				get {
-					return is_directory;
-				}
-
-				set {
-					is_directory = value;
-				}
-			}
-
-			public string Uri {
-				get {
-					return uri;
-				}
-
-				set {
-					uri = value;
-				}
-			}
-		
-			public string Comment {
-				get {
-					return comment;
-				}
+			string filename = Path.GetTempFileName ();
+			FileStream file_stream = File.OpenWrite (filename);
 			
-				set {
-					comment = value;
-				}
-			}
+			Mono.Unix.Native.Syscall.chmod (filename, (Mono.Unix.Native.FilePermissions) 384); // 384 is 0600
+			
+			BufferedStream buffered_stream = new BufferedStream (file_stream);
 
-			public string TempFileName {
-				get {
-					return temp_file_name;
-				}
+			byte [] buffer = new byte [8192];
+			int read;
 
-				set {
-					temp_file_name = value;
-				}
-			}
+			do {
+				read = stream.Read (buffer, 0, buffer.Length);
+				if (read > 0)
+					buffered_stream.Write (buffer, 0, read);
+			} while (read > 0);
 
+			buffered_stream.Close ();
+
+			File.SetLastWriteTimeUtc (filename, mtime);
+
+			Mono.Unix.Native.Syscall.chmod (filename, (Mono.Unix.Native.FilePermissions) 256); // 256 is 0400
+
+			return filename;
 		}
 
-		private class Archive : Stream
+		private ArchiveEntry GetNextEntryZip ()
 		{
-			Stream base_stream = null;
-			string uri = null;
-			string name = null;
-			string method = null;
+			ZipInputStream zip_stream = (ZipInputStream) archive_stream;
+			ZipEntry zip_entry;
 
-			delegate ArchiveEntry GetNextEntryType ();
-			GetNextEntryType getNextEntry;
-			bool first = true;
+			do {
+				zip_entry = zip_stream.GetNextEntry ();
+			} while (zip_entry != null && zip_entry.IsDirectory);
 
-			public Archive (string filename, string mimeType) {
-				this.uri = UriFu.PathToFileUriString (filename);
-				name = filename;
-				base_stream = new FileStream (filename,
-							     FileMode.Open,
-							     FileAccess.Read);
-				Init (base_stream, mimeType);
+			// End of the entries.
+			if (zip_entry == null)
+				return null;
 
-			}
+			ArchiveEntry entry = new ArchiveEntry ();
+			entry.Name = zip_entry.Name;
+			entry.Modified = zip_entry.DateTime; // FIXME: Not sure zip_entry.DateTime is UTC.
+			entry.Comment = zip_entry.Comment;
 
-			private void Init (Stream stream, string mimeType) {
+			entry.TempFile = StoreStreamInTempFile (archive_stream, entry.Modified);
+			entry.MimeType = XdgMime.GetMimeType (entry.TempFile);
 
-				switch (mimeType) {
-				case "application/zip":
-					base_stream = new ZipInputStream (base_stream);
-					getNextEntry = new GetNextEntryType (GetNextEntryZip);
-					method = "zip:";
-					break;
-				case "application/x-bzip-compressed-tar":
-					base_stream = new BZip2InputStream (base_stream);
-					base_stream = new TarInputStream (base_stream);
-					getNextEntry = new GetNextEntryType (GetNextEntryTar);
-					method = "bzip2tar:";
-					break;
-				case "application/x-compressed-tar":
-				case "application/x-tgz":
-					base_stream = new GZipInputStream (base_stream);
-					base_stream = new TarInputStream (base_stream);
-					getNextEntry = new GetNextEntryType (GetNextEntryTar);
-					method = "gziptar:";
-					break;
-				case "application/x-tar":
-					base_stream = new TarInputStream (base_stream);
-					getNextEntry = new GetNextEntryType (GetNextEntryTar);
-					method = "tar:";
-					break;
-				case "application/x-gzip":
-					base_stream = new GZipInputStream (base_stream);
-					getNextEntry = new GetNextEntryType (GetNextEntrySingle);
-					method = "gzip:";
-					break;
-				case "application/x-bzip":
-					base_stream = new BZip2InputStream (base_stream);
-					getNextEntry = new GetNextEntryType (GetNextEntrySingle);
-					method = "bzip:";
-					break;
-				default:
-					throw new ArgumentException ("Invalid or unsupported mime type.");
-				}
-			}
+			return entry;
+		}
 
-			/*
-			public string GetNextEntrySingleAsUri () {
-				if (first) {
-					first = false;
-					return uri + method;
-				} else
-					return null;
-			}
-			*/
+		private ArchiveEntry GetNextEntryTar ()
+		{
+			TarInputStream tar_stream = (TarInputStream) archive_stream;
+			TarEntry tar_entry;
 
-			/* Returns stream of the next entry */
-			public ArchiveEntry GetNextEntry () 
-			{
-				return getNextEntry ();
-			}
+			do {
+				tar_entry = tar_stream.GetNextEntry ();
+			} while (tar_entry != null && tar_entry.IsDirectory);
 
-			private String GetEntryFile (string entry_name)
-			{			
-				string temp_file = null;
-				//temp_file = Path.Combine (Path.GetTempPath (), entry_name); 
-				temp_file = Path.GetTempFileName ();
-				File.Delete (temp_file);
-				temp_file =  temp_file + Path.GetExtension (entry_name);
-				Console.WriteLine ("GetEntryFile: temp_file = {0}", temp_file);
-				FileStream filestream = File.OpenWrite (temp_file);
+			// End of the entries;
+			if (tar_entry == null)
+				return null;
 
-				BufferedStream buffered_stream = new BufferedStream (filestream);
-				BinaryWriter writer = new BinaryWriter (buffered_stream);
+			ArchiveEntry entry = new ArchiveEntry ();
+			entry.Name = tar_entry.Name;
+			entry.Modified = tar_entry.ModTime;
 
-				const int BUFFER_SIZE = 8192;
-				byte[] data = new byte [BUFFER_SIZE];
+			entry.TempFile = StoreStreamInTempFile (archive_stream, entry.Modified);
+			entry.MimeType = XdgMime.GetMimeType (entry.TempFile);
 
-				int read;
-				do {
-					read = base_stream.Read (data, 0, BUFFER_SIZE);
-					if (read > 0)
-						writer.Write (data, 0, read);
-				} while (read > 0);
-			
-				writer.Close ();
+			return entry;
+		}
 
-				File.SetLastWriteTime (temp_file, File.GetLastWriteTime (this.name));
-				return temp_file;
-			}
+		private bool handled_single = false;
 
-			public ArchiveEntry GetNextEntryZip () {
-				ArchiveEntry a_entry = null;
+		private ArchiveEntry GetNextEntrySingle ()
+		{
+			if (handled_single)
+				return null;
 
-				ZipInputStream inputStream = base_stream as ZipInputStream;
-				ZipEntry entry = inputStream.GetNextEntry();
-				if (entry != null && !entry.IsDirectory) {
-					a_entry = new ArchiveEntry ();
-					a_entry.Uri = /* DB uri + method + */entry.Name;
-					a_entry.Size = entry.Size;
-					a_entry.Modified  = entry.DateTime;
-					a_entry.IsDirectory = entry.IsDirectory;
-					a_entry.Name = entry.Name;
-					a_entry.TempFileName = GetEntryFile (entry.Name);
-					a_entry.MimeType = Beagle.Util.XdgMime.GetMimeType (a_entry.TempFileName);
-					a_entry.Comment = entry.Comment;
-				}
-				return a_entry;
-			}
+			ArchiveEntry entry = new ArchiveEntry ();
+			entry.Name = Path.GetFileNameWithoutExtension (this.file_info.Name);
+			entry.Modified = this.file_info.LastWriteTimeUtc;
 
-			public ArchiveEntry GetNextEntrySingle () {
-				if (first) {
-					first = false;
-					return null;
-				} else
-					return null;
-			}
+			entry.TempFile = StoreStreamInTempFile (archive_stream, entry.Modified);
+			entry.MimeType = XdgMime.GetMimeType (entry.TempFile);
 
-			public ArchiveEntry GetNextEntryTar () {
-				ArchiveEntry a_entry = null;
-				TarInputStream inputStream = base_stream as TarInputStream;
-				TarEntry entry = inputStream.GetNextEntry();
-				if (entry != null && !entry.IsDirectory) {
-					a_entry = new ArchiveEntry ();
-					a_entry.Uri = /* DB uri + method + */entry.Name;
-					a_entry.Size = entry.Size;
-					a_entry.Modified  = entry.ModTime;
-					a_entry.IsDirectory = entry.IsDirectory;
-					a_entry.Name = entry.Name;
-					a_entry.TempFileName = GetEntryFile (entry.Name);
-					a_entry.MimeType = Beagle.Util.XdgMime.GetMimeType (a_entry.TempFileName);
-				}
-				return a_entry;
-			}
+			handled_single = true;
 
-			public override int Read (byte[] buffer, int offset, int length) {
-				return base_stream.Read (buffer, offset, length);
-			}
-
-			public override IAsyncResult BeginRead (byte[] buffer, int offset, int length,
-								AsyncCallback cback, object state)
-			{
-				return base_stream.BeginRead (buffer, offset, length, cback, state);
-			}
-
-			public override int EndRead(IAsyncResult async_result) {
-				return base_stream.EndRead (async_result);
-			}
-
-			public override void Write (byte[] buffer, int offset, int length) {
-				throw new NotSupportedException ();
-			}
-			public override void Flush () {
-				throw new NotSupportedException ();
-			}
-			public override long Seek (long offset, SeekOrigin origin) {
-				throw new NotSupportedException ();
-			}
-			public override void SetLength (long value) {
-				throw new System.NotSupportedException();
-			}
-			public override bool CanRead {
-				get {
-					return base_stream.CanRead;
-				}
-			}
-			public override bool CanSeek {
-				get {
-					return false;
-				}
-			}
-			public override bool CanWrite {
-				get {
-					return false;
-				}
-			}
-			public override long Length {
-				get {
-					throw new System.NotSupportedException();
-				}
-			}
-			public override long Position {
-				get {
-					throw new System.NotSupportedException();
-				}
-				set {
-					throw new System.NotSupportedException();
-				}
-			}
+			return entry;
 		}
 	}	
 }
