869b9115b7b5d56116802bd181eb81a3bbbda375
Debajyoti Bera <dbera@src.gnome.org>
2007-01-05 05:38:52 +0000

    Import RTF Parser from System.Windows.Forms.RTF. Use a local copy (*sigh*) since SWF.RTF is not exposed as a public API. The modified RTF Filter will follow next. And trust me, it is far less buggy and 4 times faster even on a 10 line rtf file.
    svn path=/trunk/; revision=3222

--- beagle/Filters/System.Windows.Forms.RTF/test.cs
+++ beagle/Filters/System.Windows.Forms.RTF/test.cs
@@ -1,33 +1,65 @@
 using System;
-using System.Collections;
-using System.IO;
 using System.Text;
-
-using Beagle.Util;
-using Beagle.Daemon;
+using System.Threading;
 using System.Windows.Forms.RTF;
+using System.IO;
 
-namespace Beagle.Filters {
-
-	public class FilterRTF : Beagle.Daemon.Filter {
-		
+namespace TextTestClass {
+	public class Test {
+		static Test	test;
 		int		skip_width;
 		int		skip_count;
+		private string rtf_string = "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1033{\\fonttbl{\\f0\\fnil\\fcharset0 Microsoft Sans Serif;}}\r\n\\viewkind4\\uc1\\pard\\f0\\fs17 testing 123testiong\\par\r\n}";
+		private string rtf_string2 =	"{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1033{\\fonttbl{\\f0\\fswiss\\fcharset0 Arial;}{\\f1\\fmodern\\fprq1\\fcharset0 Courier;}{\\f2\\fswiss\\fprq2\\fcharset0 Arial;}}\r\n" + 
+			"{\\colortbl ;\\red255\\green0\\blue0;\\red0\\green0\\blue0;}\r\n" + 
+			"{\\*\\generator Msftedit 5.41.15.1507;}\\viewkind4\\uc1\\pard\\f0\\fs20 I am in Arial 10pt\\par\r\n" + 
+			"\\fs24 I am in Arial 12pt\\par\r\n" +
+			"\\f1 I am in Courier 12pt\\par\r\n" + 
+			"\\cf1 I am in Courier 12pt Red\\par\r\n" + 
+			"\\cf2\\f2\\fs20 I am in Arial 10pt\\par\r\n" +
+			"\\b I am in Arial 10pt Italic\\cf0\\b0\\f0\\par\r\n" +
+			"}";
+		private string rtf_string3 = "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang1033{\\fonttbl{\\f0\\fswiss\\fcharset0 Arial;}{" + 
+			"\\f1\\fmodern\\fprq1\\fcharset0 Courier;}{\\f2\\fswiss\\fprq2\\fcharset0 Arial;}{\\f3\\fni" +
+			"l\\fcharset0 Impact;}{\\f4\\fnil\\fcharset0 Arial Unicode MS;}{\\f5\\fnil\\fcharset136 Arial Unicode MS;}{\\f6\\fnil\\fcharset0 MS" +
+			" Shell Dlg;}}" +
+			"{\\colortbl ;\\red255\\green0\\blue0;\\red0\\green0\\blue0;}" +
+			"{\\*\\generator Msftedit 5.41.15.1507;}\\viewkind4\\uc1\\pard\\f0\\fs20 I am in Arial 1" +
+			"0pt\\par" +
+			"\\fs24 I am in Arial 12pt\\par" +
+			"\\f1 I am in Courier 12pt\\par" +
+			"\\cf1 I am in Courier 12pt Red\\par" +
+			"\\cf2\\f2\\fs20 I am in Arial 10pt\\par" +
+			"\\b I am in Arial 10pt Bold\\par" +
+			"\\i I am in Arial 10pt Bold Italic\\par" +
+			"\\ul I am in Arial 10pt Bold Italic Underline\\par" +
+			"\\ulnone\\b0\\i0\\strike I am in Arial 10pt Strikethrough\\par" +
+			"\\cf0\\strike0\\f3\\fs23 Some cyrilic character: \\u1034?\\par" +
+			"And 5 CJK characters: \\f4\\fs21\\u23854?\\u23854?\\u23854?\\u23854?\\u23854?\\f5\\fs17\\par" + 
+			"Some special chars:\\par" +
+			"\\tab Tilde: ~\\par" +
+			"\\tab Questionmark:?\\par" +
+			"\\tab Yen: \\f5\\u165?\\f6\\fs17\\par" +
+			"\\tab Umlaut: \\'e4\\par" +
+			"\\f0\\fs20\\par" +
+			"}";
+
 		TextMap text;
 
-		public FilterRTF ()
-		{
-			// Make this a general rtf filter.
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/rtf"));
-			SnippetMode = true;
-		}
-
-		override protected void DoOpen (FileInfo info) 
-		{
-			RTF rtf = new RTF (Stream);
+		public Test() {
+			MemoryStream	stream;
+			RTF		rtf;
+			byte[]		buffer;
 
 			text = new TextMap();
 			TextMap.SetupStandardTable(text.Table);
+
+			buffer = new byte[rtf_string.Length];
+			for (int i = 0; i < buffer.Length; i++) {
+				buffer[i] = (byte)rtf_string[i];
+			}
+			stream = new MemoryStream(buffer);
+			rtf = new RTF(stream);
 
 			skip_width = 0;
 			skip_count = 0;
@@ -36,10 +68,11 @@ namespace Beagle.Filters {
 			rtf.ClassCallback[TokenClass.Control] = new ClassDelegate(HandleControl);
 
 			rtf.Read();
+
+			stream.Close();
 		}
 
 		void HandleControl(RTF rtf) {
-			return;
 			switch(rtf.Major) {
 				case Major.Unicode: {
 					switch(rtf.Minor) {
@@ -242,5 +275,8 @@ namespace Beagle.Filters {
 			}
 		}
 
+		public static void Main() {
+			test = new Test();
+		}
 	}
 }
