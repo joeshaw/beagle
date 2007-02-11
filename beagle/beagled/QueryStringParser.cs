e181849b8c3f494ecf8015f0279b8fe5aada8552
Debajyoti Bera <dbera@src.gnome.org>
2007-02-11 21:50:46 +0000

    Allow date queries. The format is  query := "date":s[-s]  s     := empty | year | year.month | year.month.date e.g. date:2006, date:2005.04.11-2007
    How to handle i18n of "date" in the GUI ?
    The UI should also support date:today, date:this_year etc. but they should be handled in the frontend itself and not in the API.
    
    svn path=/trunk/; revision=3423

--- beagle/beagled/QueryStringParser.cs
+++ beagle/beagled/QueryStringParser.cs
@@ -187,11 +187,23 @@ namespace Beagle.Daemon {
 				return query_part; 
 			}
 
-			if (key == "") {
+			if (key == String.Empty) {
 
 				Logger.Log.Debug ("Parsed query '{0}' as text_query", text);
 
 				return StringToQueryPart (text, IsProhibited);
+			}
+
+			// FIXME: i18n-izing "date"
+			if (key == "date") {
+				try {
+					QueryPart part = DateQueryToQueryPart (text);
+					part.Logic = (IsProhibited ? QueryPartLogic.Prohibited : QueryPartLogic.Required);
+					return part;
+				} catch (FormatException) {
+					Log.Debug ("Could not parse [{0}] as date query. Assuming text.", text);
+					return StringToQueryPart (text, IsProhibited);
+				}
 			}
 
 			string prop_string = null;
@@ -224,6 +236,94 @@ namespace Beagle.Daemon {
 					  " and property type="	    + query_part_prop.Type);
 
 			return query_part_prop;
+		}
+
+		private static QueryPart DateQueryToQueryPart (string query)
+		{
+			// Format
+			// query := date[-date]
+			// date := empty | year | year.month | year.month.date 
+			// FIXME: Do we really want to query time too ? They are too long!
+
+			if (query == String.Empty)
+				throw new FormatException ();
+
+			int next_date_index = query.IndexOf ('-');
+			if (next_date_index == -1)
+				return DateToQueryPart (query);
+
+			// We have a range query - get the ranges
+			DateTime start_date, end_date;
+
+			try {
+				int y, m, d;
+				ParseDateQuery (query.Substring (0, next_date_index), out y, out m, out d);
+				start_date = CreateDateTime (y, m, d, true);
+			} catch (FormatException) {
+				start_date = DateTime.MinValue;
+			}
+				
+			try {
+				int y, m, d;
+				ParseDateQuery (query.Substring (next_date_index + 1), out y, out m, out d);
+				end_date = CreateDateTime (y, m, d, false);
+			} catch (FormatException) {
+				// FIXME: Should the default end_date be DateTime.Now ?
+				end_date = DateTime.MinValue;
+			}
+
+			if (start_date == DateTime.MinValue && end_date == DateTime.MinValue)
+				throw new FormatException ();
+				
+			QueryPart_DateRange range_query = new QueryPart_DateRange ();
+			range_query.Key = QueryPart_DateRange.TimestampKey;
+			range_query.StartDate = start_date;
+			range_query.EndDate = end_date;
+			Log.Debug ("Parsed date range query [{0}] as {1}", query, range_query);
+
+			return range_query;
+		}
+
+		private static void ParseDateQuery (string dt_string, out int year, out int month, out int date)
+		{
+			year = month = date = -1;
+
+			if (dt_string.Length >= 4)
+				year = Convert.ToInt32 (dt_string.Substring (0, 4));
+
+			if (dt_string.Length >= 6)
+				month = Convert.ToInt32 (dt_string.Substring (4, 2));
+
+			if (dt_string.Length == 8)
+				date = Convert.ToInt32 (dt_string.Substring (6, 2));
+
+			if (dt_string.Length > 8 || year == -1)
+				throw new FormatException ();
+		}
+
+		private static DateTime CreateDateTime (int y, int m, int d, bool start_date)
+		{
+			if (m == -1)
+				m = (start_date ? 1 : 12);
+			if (d == -1)
+				d = (start_date ? 1 : DateTime.DaysInMonth (y, m));
+
+			// Create the date in local time
+			return new DateTime (y, m, d, 0, 0, 0, DateTimeKind.Local);
+		}
+
+		private static QueryPart_DateRange DateToQueryPart (string dt_string)
+		{
+			int y, m, d;
+			ParseDateQuery (dt_string, out y, out m, out d);
+
+			QueryPart_DateRange dt_query = new QueryPart_DateRange ();
+			dt_query.Key = QueryPart_DateRange.TimestampKey;
+			dt_query.StartDate = CreateDateTime (y, m, d, true);
+			dt_query.EndDate =  CreateDateTime (y, m, d, false);
+			Log.Debug ("Parsed date query [{0}] as {1}", dt_string, dt_query);
+
+			return dt_query;
 		}
 	}
 }
