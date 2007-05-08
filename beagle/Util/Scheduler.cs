e9e29d396f2d3767d8daf9081dcee262ad9cf9a6
Joe Shaw <joeshaw@src.gnome.org>
2007-05-08 19:16:26 +0000

    Merge in changes to ChangeLog and NEWS from the branch back to the trunk (r3698-3700)
    svn path=/trunk/; revision=3701

--- beagle/Util/Scheduler.cs
+++ beagle/Util/Scheduler.cs
@@ -39,7 +39,7 @@ namespace Beagle.Util {
 		public delegate void EmptyQueueDelegate ();
 		public event EmptyQueueDelegate EmptyQueueEvent;
 
-		public static bool Debug = false;
+		public static bool Debug = true;
 
 		public enum Priority {
 
