e927885d7d5d7aa3cd11aaf22b282b1d15d42def
Debajyoti Bera <dbera@src.gnome.org>
2007-05-10 04:01:50 +0000

    Use taglib-sharp instead of entagged-sharp. Give this a few tests and then entagged-sharp will be removed completely.
    svn path=/trunk/; revision=3706

--- beagle/Filters/FilterMusic.cs
+++ beagle/Filters/FilterMusic.cs
@@ -1,6 +1,6 @@
 //
-// FilterMusic.cs : This is our interface to entagged-sharp's AudioFileWrapper
-//                  interface.
+// FilterMusic.cs : This is our interface to taglib-sharp's interface.
+// Copyright (C) 2007 Debajyoti Bera <dbera.web@gmail.com>
 //
 
 //
@@ -27,7 +27,7 @@ using System;
 using System.IO;
 using Beagle.Daemon;
 using Beagle.Util;
-using Entagged;
+using TagLib;
 
 namespace Beagle.Filters {
 
@@ -45,84 +45,94 @@ namespace Beagle.Filters {
 
 		protected override void RegisterSupportedTypes ()
 		{
-			// APE / Monkeys Audio
-			AddSupportedFlavor (FilterFlavor.NewFromExtension (".ape"));
-
-			// FLAC
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-flac"));
-
-			// MP3
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-mp3"));
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/mpeg"));
-
-			// MPC / Musepack / MPEG+
-			AddSupportedFlavor (FilterFlavor.NewFromExtension (".mpc"));
-			AddSupportedFlavor (FilterFlavor.NewFromExtension (".mp+"));
-
-			// M4A / Apple Audio Codec
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-m4a"));
-			AddSupportedFlavor (FilterFlavor.NewFromExtension (".m4p"));
-
-			// OGG Vorbis
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("application/ogg"));
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-vorbis+ogg"));
-
-			// Tracker / Amiga Audio
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-s3m"));
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-it"));
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-mod"));
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-xm"));
-
-			// ASF / WMA
-			AddSupportedFlavor (FilterFlavor.NewFromMimeType ("audio/x-ms-wma"));
+			foreach(string type in SupportedMimeType.AllMimeTypes)
+				AddSupportedFlavor (FilterFlavor.NewFromMimeType (type));
 		}
 
-		private string GetEntaggedMimeType ()
+		private string GetTaglibMimeType ()
 		{
 			if (Extension != null && Extension.Length > 0)
-				return "entagged/" + Extension.Substring (1);
+				return "taglib/" + Extension.Substring (1);
 			else
 				return MimeType;
 		}
 
+		internal class FilterMusicFileAbstraction : TagLib.File.IFileAbstraction
+		{
+			private Stream stream;
+
+			public FilterMusicFileAbstraction(Stream stream)
+			{
+				this.stream = stream;
+			}
+			
+			public string Name {
+				get { return null; }
+			}
+
+			public System.IO.Stream ReadStream {
+				get { return stream; }
+			}
+
+			public System.IO.Stream WriteStream {
+				get { throw new Exception ("Not supported"); }
+			}
+		}
+
 		protected override void DoPullProperties ()
 		{
-			AudioFile tag;
+			TagLib.File file;
 			
 			try {
-				tag = new AudioFile (Stream, GetEntaggedMimeType ());
+				file = TagLib.File.CreateReadOnly (Stream, GetTaglibMimeType ());
 			} catch (Exception e) {
 				Logger.Log.Warn (e, "Exception filtering music");
 				Finished();
 				return;
 			}
 
-			foreach (string artist in tag.Artists)
+			TagLib.Tag tag = file.Tag;
+
+			AddProperty (Beagle.Property.New ("fixme:album", tag.Album));
+			AddProperty (Beagle.Property.New ("dc:title", tag.Title));
+
+			foreach (string artist in tag.AlbumArtists)
 				AddProperty (Beagle.Property.New ("fixme:artist", artist));
 
-			foreach (string album in tag.Albums)
-				AddProperty (Beagle.Property.New ("fixme:album", album));
+			foreach (string performer in tag.Performers)
+				AddProperty (Beagle.Property.New ("fixme:performer", performer));
 
-			foreach (string title in tag.Titles)
-				AddProperty (Beagle.Property.New ("fixme:title", title));
-
-			foreach (string comment in tag.Comments)
-				AddProperty (Beagle.Property.New ("fixme:comment", comment));
-
-			foreach (int track in tag.TrackNumbers)
-				AddProperty (Beagle.Property.NewUnsearched ("fixme:tracknumber", track));
-
-			foreach (int track in tag.TrackCounts)
-				AddProperty (Beagle.Property.NewUnsearched ("fixme:trackcount", track));
-
-			foreach (int year in tag.Years)
-				AddProperty (Beagle.Property.NewUnsearched ("fixme:year", year));
+			foreach (string composer in tag.Composers)
+				AddProperty (Beagle.Property.New ("fixme:composer", composer));
 
 			foreach (string genre in tag.Genres)
-				AddProperty (Beagle.Property.NewKeyword ("fixme:genre", genre));
+				AddProperty (Beagle.Property.New ("fixme:genre", genre));
 
-			AddProperty (Beagle.Property.NewUnsearched ("fixme:duration", tag.Duration));
-			AddProperty (Beagle.Property.NewUnsearched ("fixme:bitrate", tag.Bitrate));
+			AddProperty (Beagle.Property.New ("fixme:comment", tag.Comment));
+			AddProperty (Beagle.Property.NewUnsearched ("fixme:tracknumber", tag.Track));
+			AddProperty (Beagle.Property.NewUnsearched ("fixme:trackcount", tag.TrackCount));
+			AddProperty (Beagle.Property.NewUnsearched ("fixme:disknumber", tag.Disc));
+			AddProperty (Beagle.Property.NewUnsearched ("fixme:diskcount", tag.DiscCount));
+			AddProperty (Beagle.Property.NewUnsearched ("fixme:year", tag.Year));
+
+			foreach (TagLib.ICodec codec in file.Properties.Codecs) {
+				TagLib.IAudioCodec acodec = codec as TagLib.IAudioCodec;
+				
+				if (acodec != null && (acodec.MediaTypes & TagLib.MediaTypes.Audio) != TagLib.MediaTypes.Unknown)
+				{
+					AddProperty (Beagle.Property.NewUnsearched ("fixme:bitrate", acodec.AudioBitrate));
+					AddProperty (Beagle.Property.NewUnsearched ("fixme:samplerate", acodec.AudioSampleRate));
+					AddProperty (Beagle.Property.NewUnsearched ("fixme:channels", acodec.AudioChannels));
+					// One codec is enough
+					break;
+				}
+				// FIXME: Get data from IVideoCodec too
+                	}
+
+                	if (file.Properties.MediaTypes != TagLib.MediaTypes.Unknown)
+				AddProperty (Beagle.Property.NewUnsearched ("fixme:duration", file.Properties.Duration));
+
+			// FIXME: Store embedded picture and lyrics
 
 			Finished ();
 		}
