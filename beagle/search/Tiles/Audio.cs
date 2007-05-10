a04afead5596e7b81c1ef4601990b488d240f854
Joe Shaw <joeshaw@src.gnome.org>
2007-05-10 19:03:08 +0000

    Check for both dc:title and fixme:title in the Audio tile.  The former is used with the new taglib-sharp and the latter with the older entagged-sharp.
    svn path=/trunk/; revision=3715

--- beagle/search/Tiles/Audio.cs
+++ beagle/search/Tiles/Audio.cs
@@ -25,12 +25,15 @@ namespace Search.Tiles {
 
 			// FIXME: Show album art if any. Needs implementation in Beagle.Util
 
-			string title = Hit.GetFirstProperty ("fixme:title");
-			if (title != null && title != "")
+			string title = Hit.GetFirstProperty ("dc:title");
+			if (String.IsNullOrEmpty (title))
+				title = Hit.GetFirstProperty ("fixme:title");
+
+			if (! String.IsNullOrEmpty (title))
 				Title = title;
 
 			string artist = Hit.GetFirstProperty ("fixme:artist");
-			if (artist != null && artist != "")
+			if (! String.IsNullOrEmpty (artist))
 				Description = artist;
 
 			//AddAction (new TileAction (Catalog.GetString ("Add to Library"), AddToLibrary));
