joeshaw/beagle

commit c42aad7bc06308c307f7ef67d7fea4458df942ed

Merge from tbird-soc07 branch r3824.
svn path=/trunk/; revision=3830

beagle/search/Tiles/MailMessage.cs
@@ -116,35 +116,32 @@ namespace Search.Tiles {
 			return details;
 		}
 
-		public static SafeProcess GetClientProcess (string client, string uri)
+		public static SafeProcess GetClientProcess (Beagle.Hit hit)
 		{
+			string client = null;
 			SafeProcess p = null;
-
+			
+			if (hit.ParentUri != null)
+				client = Utils.GetFirstPropertyOfParent (hit, "fixme:client");
+			else
+				client = hit.GetFirstProperty ("fixme:client");
+			
 			if (client == "evolution") {
 				p = new SafeProcess ();
 				p.Arguments = new string [2];
 				p.Arguments [0] = "evolution";
-				p.Arguments [1] = uri;
+				p.Arguments [1] = (hit.ParentUri != null ? hit.EscapedParentUri : hit.EscapedUri);
 #if ENABLE_THUNDERBIRD
-			} else if (client == "thunderbird") {
-				p = new SafeProcess ();
-				p.Arguments = new string [3];
-				p.Arguments [0] = Thunderbird.ExecutableName;
-				p.Arguments [1] = "-mail";
-				p.Arguments [2] = uri;
+			} else if (client == "thunderbird")
+				p = Thunderbird.GetSafeProcess ("-viewbeagle", hit.GetFirstProperty ("fixme:uri"));
 #endif
-			}
 
 			return p;
 		}
 
 		public override void Open ()
 		{
-			SafeProcess p;
-			if (Hit.ParentUri != null) 
-				p = GetClientProcess (Utils.GetFirstPropertyOfParent (Hit, "fixme:client"), Hit.EscapedParentUri);
-			else
-				p = GetClientProcess (Utils.GetFirstPropertyOfParent (Hit, "fixme:client"), Hit.EscapedUri);
+			SafeProcess p = GetClientProcess (Hit);
 			
 			if (p == null) {
 				OpenFromMime (Hit);

beagle/search/Tiles/RSSFeed.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Diagnostics;
 using System.Collections;
 using Mono.Unix;
+using Beagle.Util;
 
 namespace Search.Tiles {
 
@@ -65,7 +66,20 @@ namespace Search.Tiles {
 
 		public override void Open ()
 		{
-			base.OpenFromUri (Hit ["dc:identifier"]);
+			// If we are not a feed from Thunderbird just open based on mime
+			if (Hit.GetFirstProperty ("fixme:client") != "thunderbird") {
+				base.OpenFromUri (Hit ["dc:identifier"]);
+				return;
+			}
+			
+			// Here's the Thunderbird specific part
+			SafeProcess p = Thunderbird.GetSafeProcess ("-viewbeagle", Hit.GetFirstProperty ("fixme:uri"));
+			
+			try {
+				p.Start ();
+			} catch (SafeProcessException e) {
+				Console.WriteLine ("Unable to run {0}: {1}", p.Arguments [0], e.Message);
+			}
 		}
 	}
 }

beagle/Makefile.am
@@ -41,9 +41,7 @@ SUBDIRS += bludgeon
 endif
 
 if ENABLE_THUNDERBIRD
-if ENABLE_GUI
-SUBDIRS += ContactViewer
-endif
+SUBDIRS += thunderbird-extension
 endif
 
 # When the chooser work has been fixed up to use libbeagle, we can

beagle/configure.in
@@ -192,9 +192,9 @@ LIBS="$OLD_LIBS"
 
 dnl ----------------------------------------------
 AC_ARG_ENABLE([thunderbird],
-	AC_HELP_STRING([--enable-thunderbird], [Enable Thunderbird Mail Backend (default no)]),
+	AC_HELP_STRING([--enable-thunderbird], [Enable Thunderbird Mail Backend (default yes)]),
 	enable_tbird=$enableval,
-	enable_tbird=no)
+	enable_tbird=yes)
 AM_CONDITIONAL(ENABLE_THUNDERBIRD, test "x$enable_tbird" = "xyes")
 
 dnl -------------------------------------------------------
@@ -578,9 +578,9 @@ tools/beagle-settings.desktop.in
 search/Makefile
 search/beagle-search.desktop.in
 ImLogViewer/Makefile
-ContactViewer/Makefile
 epiphany-extension/Makefile
 mozilla-extension/Makefile
+thunderbird-extension/Makefile
 bludgeon/Makefile
 beagle-0.0.pc
 beagle-daemon.pc

beagle/search/Tiles/MailAttachment.cs
@@ -67,12 +67,8 @@ namespace Search.Tiles {
 
 		public override void Open ()
 		{
-			SafeProcess p;
+			SafeProcess p = MailMessage.GetClientProcess (Hit);
 			
-			if (Hit.ParentUri != null)
-				p = MailMessage.GetClientProcess (GetHitProperty (Hit, "fixme:client"), Hit.EscapedParentUri);
-			else
-				p = MailMessage.GetClientProcess (GetHitProperty (Hit, "fixme:client"), Hit.EscapedUri);
 			if (p == null) {
 				OpenFromMime (Hit);
 				return;

beagle/thunderbird-extension/Makefile.am
@@ -1,0 +1,65 @@
+PROJECT = beagle
+VERSION = 0.1
+
+XPIDL_BIN = /usr/lib/firefox/xpidl
+IDL_FILES = /usr/share/idl/mozilla-thunderbird
+
+all: $(PROJECT).xpi
+
+JAR_FILES =					\
+	content/beagle.css			\
+	content/beagleIndexer.js		\
+	content/beagleMailWindow.xul		\
+	content/beagleMessenger.xul		\
+	content/beaglePrefs.xul			\
+	content/beagleQueue.js			\
+	content/beagleService.js		\
+	content/beagleSettings.js		\
+	content/beagleUnindex.js		\
+	content/beagleUnindex.xul		\
+	content/contents.rdf			\
+	locale/en-US/beagle.dtd			\
+	locale/en-US/strings.properties		\
+	locale/en-US/contents.rdf		\
+	skin/classic/overlay.css		\
+	skin/classic/beagle.png			\
+	skin/classic/beagle-disabled.png	\
+	skin/classic/beagle-error.png		\
+	skin/classic/contents.rdf
+
+COMPONENT_FILES = 				\
+        components/BeagleIndexer.xpt		\
+        components/BeagleQueue.xpt		\
+        components/BeagleSettings.xpt
+
+XPI_FILES =					\
+	chrome.manifest				\
+	install.rdf				\
+	chrome/$(PROJECT).jar			\
+	defaults/preferences/default.js		\
+	components/BeagleIndexer.js		\
+	components/BeagleQueue.js		\
+	components/BeagleSettings.js		\
+	components/BeagleCommandLine.js		\
+	$(COMPONENT_FILES)
+
+chrome/$(PROJECT).jar: $(JAR_FILES)
+	-@mkdir -p chrome
+	zip -q9 $@ $^
+
+$(COMPONENT_FILES): 
+	-@mkdir -p components 
+	$(XPIDL_BIN) -m typelib -w -v -I $(IDL_FILES) -e $@ `echo $@ | sed s/xpt/idl/`
+
+$(PROJECT).xpi: $(XPI_FILES)
+	zip -q9 $@ $^
+
+EXTRA_DIST =					\
+	$(JAR_FILES)				\
+	$(COMPONENT_FILES)			\
+	$(XPI_FILES)
+
+CLEANFILES =					\
+	chrome/$(PROJECT).jar			\
+	components/*.xpt			\
+	$(PROJECT).xpi				

beagle/thunderbird-extension/chrome.manifest
@@ -1,0 +1,5 @@
+content 	beagle 	jar:chrome/beagle.jar!/content/
+overlay 	chrome://messenger/content/messenger.xul chrome://beagle/content/beagleMessenger.xul
+overlay		chrome://messenger/content/mailWindowOverlay.xul chrome://beagle/content/beagleMailWindow.xul
+locale 		beagle 	en-US 		jar:chrome/beagle.jar!/locale/en-US/
+skin 		beagle 	classic/1.0 	jar:chrome/beagle.jar!/skin/classic/

beagle/thunderbird-extension/components/BeagleCommandLine.js
@@ -1,0 +1,182 @@
+//
+// beagle.xul: Overlay for basic main window GUI items
+//
+// Copyright (C) 2007 Pierre Östlund
+//
+
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+
+// Original source: http://developer.mozilla.org/en/docs/Chrome:_Command_Line
+
+const nsIAppShellService    = Components.interfaces.nsIAppShellService;
+const nsISupports           = Components.interfaces.nsISupports;
+const nsICategoryManager    = Components.interfaces.nsICategoryManager;
+const nsIComponentRegistrar = Components.interfaces.nsIComponentRegistrar;
+const nsICommandLine        = Components.interfaces.nsICommandLine;
+const nsICommandLineHandler = Components.interfaces.nsICommandLineHandler;
+const nsIFactory            = Components.interfaces.nsIFactory;
+const nsIModule             = Components.interfaces.nsIModule;
+const nsIWindowWatcher      = Components.interfaces.nsIWindowWatcher;
+
+const clh_contractID = "@mozilla.org/commandlinehandler/general-startup;1?type=beagle";
+const clh_CID = Components.ID("{679f520a-b062-45f9-a02c-482cfebf3b77}");
+const clh_category = "m-beagle";
+
+var loaded = false;
+var last_uri = null;
+
+const startupObserver = {
+
+	Observer: Components.classes['@mozilla.org/observer-service;1']
+		.getService(Components.interfaces.nsIObserverService),
+
+	RegisterSelf: function ()
+	{
+		this.Observer.addObserver (this, 'beagle-loaded', false);
+		this.Observer.addObserver (this, 'quit-application', false);
+	},
+	
+	UnregisterSelf: function ()
+	{
+		this.Observer.removeObserver (this, 'beagle-loaded');
+		this.Observer.removeObserver (this, 'quit-application');
+	},
+	
+	notify: function ()
+	{
+		if (!loaded || !last_uri)
+			return;
+		
+		var uri = last_uri;
+		last_uri = null;
+		this.Observer.notifyObservers (this, 'beagle-open-uri', uri);
+	},
+
+	observe: function (subject, topic, data)
+	{
+		if (topic == 'beagle-loaded') {
+			loaded = true;
+			this.notify ();
+		} else if (topic == 'quit-application')
+			this.UnregisterSelf ();
+	}
+};
+ 
+const myAppHandler = {
+
+	QueryInterface : function clh_QI(iid)
+	{
+		if (iid.equals(nsICommandLineHandler) || iid.equals(nsIFactory) || iid.equals(nsISupports))
+			return this;
+
+		throw Components.results.NS_ERROR_NO_INTERFACE;
+	},
+
+	handle : function clh_handle(cmdLine)
+	{
+		try {
+			var uristr = cmdLine.handleFlagWithParam("viewbeagle", false);
+			if (uristr) {
+				last_uri = uristr;
+				cmdLine.preventDefault = false;
+				startupObserver.notify ();
+			}
+		} catch (e) {
+			Components.utils.reportError("incorrect parameter passed to -viewbeagle on the command line.");
+		}
+	},
+
+	helpInfo : "  -viewbeagle <uri>       Open specified URI,\n",
+
+	createInstance : function clh_CI(outer, iid)
+	{
+		if (outer != null)
+			throw Components.results.NS_ERROR_NO_AGGREGATION;
+
+		return this.QueryInterface(iid);
+	},
+
+	lockFactory : function clh_lock(lock)
+	{
+	}
+};
+
+
+const myAppHandlerModule = {
+
+	QueryInterface : function mod_QI(iid)
+	{
+		if (iid.equals(nsIModule) || iid.equals(nsISupports))
+			return this;
+
+		throw Components.results.NS_ERROR_NO_INTERFACE;
+	},
+
+	getClassObject : function mod_gch(compMgr, cid, iid)
+	{
+		if (cid.equals(clh_CID))
+			return myAppHandler.QueryInterface(iid);
+
+		throw Components.results.NS_ERROR_NOT_REGISTERED;
+	},
+
+	registerSelf : function mod_regself(compMgr, fileSpec, location, type)
+	{
+		compMgr.QueryInterface(nsIComponentRegistrar);
+
+		compMgr.registerFactoryLocation(clh_CID,
+			"myAppHandler",
+			clh_contractID,
+			fileSpec,
+			location,
+			type);
+
+		var catMan = Components.classes["@mozilla.org/categorymanager;1"]
+			.getService(nsICategoryManager);
+		catMan.addCategoryEntry("command-line-handler",
+			clh_category,
+			clh_contractID, true, true);
+	},
+
+	unregisterSelf : function mod_unreg(compMgr, location, type)
+	{
+		compMgr.QueryInterface(nsIComponentRegistrar);
+		compMgr.unregisterFactoryLocation(clh_CID, location);
+
+		var catMan = Components.classes["@mozilla.org/categorymanager;1"]
+			.getService(nsICategoryManager);
+		catMan.deleteCategoryEntry("command-line-handler", clh_category);
+	},
+
+	canUnload : function (compMgr)
+	{
+		return true;
+	}
+};
+
+function NSGetModule(comMgr, fileSpec)
+{
+	return myAppHandlerModule;
+}
+
+startupObserver.RegisterSelf ();
+
+

commit ee0fc82079eda5c1e25d2b97cbff8e926a6daf52

Fix a broken build introduced by r3830.
svn path=/trunk/; revision=3835

beagle/search/Tiles/MailMessage.cs
@@ -131,9 +131,11 @@ namespace Search.Tiles {
 				p.Arguments = new string [2];
 				p.Arguments [0] = "evolution";
 				p.Arguments [1] = (hit.ParentUri != null ? hit.EscapedParentUri : hit.EscapedUri);
+			}
 #if ENABLE_THUNDERBIRD
-			} else if (client == "thunderbird")
-				p = Thunderbird.GetSafeProcess ("-viewbeagle", hit.GetFirstProperty ("fixme:uri"));
+			else if (client == "thunderbird")  {
+			     p = Thunderbird.GetSafeProcess ("-viewbeagle", hit.GetFirstProperty ("fixme:uri"));
+			}
 #endif
 
 			return p;

beagle/search/Tiles/RSSFeed.cs
@@ -71,7 +71,8 @@ namespace Search.Tiles {
 				base.OpenFromUri (Hit ["dc:identifier"]);
 				return;
 			}
-			
+
+#if ENABLE_THUNDERBIRD			
 			// Here's the Thunderbird specific part
 			SafeProcess p = Thunderbird.GetSafeProcess ("-viewbeagle", Hit.GetFirstProperty ("fixme:uri"));
 			
@@ -80,6 +81,7 @@ namespace Search.Tiles {
 			} catch (SafeProcessException e) {
 				Console.WriteLine ("Unable to run {0}: {1}", p.Arguments [0], e.Message);
 			}
+#endif
 		}
 	}
 }