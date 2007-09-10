bb5cc2d741a24f8df1e4307ad70f8569fed71b86
Debajyoti Bera <dbera@src.gnome.org>
2007-09-10 15:19:28 +0000

    Make the example simpler and use query_part_human for complex queries.
    svn path=/trunk/; revision=3951

--- libbeagle/wrappers/python/examples/beagle-async-snippet.py
+++ libbeagle/wrappers/python/examples/beagle-async-snippet.py
@@ -12,9 +12,15 @@ class TestCase:
 		self.beagle = beagle.Client()
 
 	def beagle_query(self, qstring):
-		print "Send query"
+		print "Send query (%s)" % qstring
+		self.hits = {}
+		self.finished = False
+
 		self.beagle_query = beagle.Query()
-		self.beagle_query.add_text(qstring)
+		query_part_human = beagle.QueryPartHuman()
+		query_part_human.set_string(qstring)
+		self.beagle_query.add_part(query_part_human)
+
 		self.beagle_query.connect("hits-added", self.hits_added_cb, qstring)
 		self.beagle_query.connect("finished", self.finished_cb)
 		self.beagle.send_request_async(self.beagle_query)
@@ -22,38 +28,26 @@ class TestCase:
 	def hits_added_cb (self, query, response, qstring):
 		hits = response.get_hits()
 		num_matches = response.get_num_matches()
-		self.hits = {}
-		self.finished = False
 	
 		print "Returned hits (%d) out of total %d matches:" % (len(hits), num_matches)
 		print "-------------------------------------------"
 	
 		for hit in hits:
-			if hit.get_type() == "FeedItem":
-				text = hit ['dc:title']
-				print "Blog: %s" % text
-			elif hit.get_type() == "File":
-				print "File: %s" % hit.get_uri()
-			elif hit.get_type() == "MailMessage":
-				print "Email: %s" % hit ['dc:title']
-				for sender in hit.get_properties ('fixme:from'):
-					print "\tSent by: %s" % sender
-			else:
-				print "%s (%s)" % (hit.get_uri(), hit.get_source())
+			print hit.get_uri()
 
 			snippet_request = beagle.SnippetRequest()
 			snippet_request.set_query(query)
 			snippet_request.set_hit(hit)
 			hit.ref()
-			snippet_request.connect('response', self._on_snippet_received)
+			snippet_request.connect('response', self._on_snippet_received, hit)
 			snippet_request.connect('closed', self._on_snippet_closed, hit)
 			self.beagle.send_request_async(snippet_request)
 			self.hits [hit] = snippet_request
 
 		print "-------------------------------------------"
    
-	def _on_snippet_received(self, request, response):
-		print "Snippet received"
+	def _on_snippet_received(self, request, response, hit):
+		print "Snippet received for %s" % hit.get_uri()
 		snippet = response.get_snippet()
 		print snippet
     
