0a0cfdb67f92aa87504c88a51025374038907e0a
Debajyoti Bera <dbera@src.gnome.org>
2007-10-02 14:57:43 +0000

    More changes from Nirbheek Chauhan. - The category checkboxes finally work exactly like they're supposed to (default.js:toggle_category, the javascript took surprisingly long), Note that when hiding all the results, `<div id="results">` is set to `display: none` instead of every category separately. - Send out a "No Results" if there are no results (default.js:157) - Each Hit can be individually shown/hidden (default.js:toggle_hit, queryresult.xsl:92), this needs to be beautified a bit. Also, the way in which toggle_hit shows/hides the Hit Data is dependant on the structure of the Hit, is this okay? Or should it be changed? (default.js:220,224) - Have the topbar be shown only when a search is being done - Slight formatting changes to the footer to bring the more important things into focus ;) (query.html:id="footer") - Convert the Beagle Logo image into a link back to a blank search page (query.html:67, default.css:41) - Added a TODO file, it lists all that I know needs to be done, we can add things to it to keep a track of what all needs to be done :)
    svn path=/trunk/; revision=4014

--- beagle/beagled/webinterface/default.js
+++ beagle/beagled/webinterface/default.js
@@ -24,156 +24,244 @@
  */
 
 function search() {
-    // get the search string
-    var query_str=document.queryform.querytext.value;
-    // FIXME: Escape query_str
-    if (query_str.length == 0)
-	return;
+	// get the search string
+	var query_str=document.queryform.querytext.value;
+	// FIXME: Escape query_str
+	if (query_str.length == 0)
+		return;
 
-    var req_string = '<?xml version="1.0" encoding="utf-8"?> <RequestWrapper xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"> <Message xsi:type="Query"> <IsIndexListener>false</IsIndexListener> <Parts> <Part xsi:type="QueryPart_Human"> <Logic>Required</Logic> <QueryString>' + query_str + '</QueryString> </Part> </Parts> <MimeTypes /> <HitTypes /> <Sources /> <QueryDomain>Local System</QueryDomain> <MaxHits>100</MaxHits> </Message> </RequestWrapper> ';
+	var req_string = '<?xml version="1.0" encoding="utf-8"?> <RequestWrapper xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"> <Message xsi:type="Query"> <IsIndexListener>false</IsIndexListener> <Parts> <Part xsi:type="QueryPart_Human"> <Logic>Required</Logic> <QueryString>' + query_str + '</QueryString> </Part> </Parts> <MimeTypes /> <HitTypes /> <Sources /> <QueryDomain>Local System</QueryDomain> <MaxHits>100</MaxHits> </Message> </RequestWrapper> ';
 
-    var begin_date = Date.now();
+	var begin_date = Date.now();
 
-    xmlhttp.onreadystatechange = function () {state_change_search (begin_date);};
-    xmlhttp.open("POST", "/", true);
-    //XHR binary charset opt by mgran 2006 [http://mgran.blogspot.com]
-    xmlhttp.overrideMimeType('text/txt; charset=utf-8'); // if charset is changed, need to handle bom
-    //xmlhttp.overrideMimeType('text/txt; charset=x-user-defined');
-    xmlhttp.send(req_string);
+	xmlhttp.onreadystatechange = function () {state_change_search (begin_date);};
+	xmlhttp.open("POST", "/", true);
+	//XHR binary charset opt by mgran 2006 [http://mgran.blogspot.com]
+	xmlhttp.overrideMimeType('text/txt; charset=utf-8'); // if charset is changed, need to handle bom
+	//xmlhttp.overrideMimeType('text/txt; charset=x-user-defined');
+	xmlhttp.send(req_string);
 
-    document.queryform.querytext.disabled=true;
-    document.getElementById('status').style.display='block';
-    return false;
+	document.queryform.querytext.disabled=true;
+	document.getElementById('status').style.display='block';
+	return false;
 }
 
 function get_information() {
-    var req_string = '<?xml version="1.0" encoding="utf-8"?><RequestWrapper xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><Message xsi:type="DaemonInformationRequest"> <GetVersion>true</GetVersion><GetSchedInfo>true</GetSchedInfo><GetIndexStatus>true</GetIndexStatus> <GetIsIndexing>true</GetIsIndexing></Message></RequestWrapper>';
+	var req_string = '<?xml version="1.0" encoding="utf-8"?><RequestWrapper xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><Message xsi:type="DaemonInformationRequest"> <GetVersion>true</GetVersion><GetSchedInfo>true</GetSchedInfo><GetIndexStatus>true</GetIndexStatus> <GetIsIndexing>true</GetIsIndexing></Message></RequestWrapper>';
 
-    xmlhttp.onreadystatechange = state_change_info;
-    xmlhttp.open("POST", "/", true);
-    //XHR binary charset opt by mgran 2006 [http://mgran.blogspot.com]
-    xmlhttp.overrideMimeType('text/txt; charset=utf-8'); // if charset is changed, need to handle bom
-    //xmlhttp.overrideMimeType('text/txt; charset=x-user-defined');
-    xmlhttp.send(req_string);
+	xmlhttp.onreadystatechange = state_change_info;
+	xmlhttp.open("POST", "/", true);
+	//XHR binary charset opt by mgran 2006 [http://mgran.blogspot.com]
+	xmlhttp.overrideMimeType('text/txt; charset=utf-8'); // if charset is changed, need to handle bom
+	//xmlhttp.overrideMimeType('text/txt; charset=x-user-defined');
+	xmlhttp.send(req_string);
 
-    document.queryform.querytext.disabled=true;
-    document.getElementById('status').style.display='block';
-    return false;
+	document.queryform.querytext.disabled=true;
+	document.getElementById('status').style.display='block';
+	return false;
 }
 
 function onError(e)
 {
-    alert("Error " + e.target.status + " occurred while receiving the document.");
+	alert("Error " + e.target.status + " occurred while receiving the document.");
 }
 
 var ii=0
 
 function state_change_search(begin_date)
 {
-    //alert("help");
-    if (xmlhttp.readyState==4) {
-	// FIXME: Should also check for status 200
+	if (xmlhttp.readyState == 4) {
+		// FIXME: Should also check for status 200
 
-	var end_date = Date.now();
-	var elapsed = (end_date - begin_date)/1000;
+		var end_date = Date.now();
+		var elapsed = (end_date - begin_date)/1000;
 
-	//dump("Response:\n");
-	//dump(xmlhttp.responseText);
-	//dump("\n");
-	res = xmlhttp.responseText;
+		//dump("Response:\n");
+		//dump(xmlhttp.responseText);
+		//dump("\n");
+		res = xmlhttp.responseText;
 
-	//alert(res.charCodeAt (0));
-	//alert(res.charCodeAt (1));
-	//alert(res.charCodeAt (2));
-	//var debug_index = res.indexOf ('</ResponseWrapper>');
-	//alert(res.charCodeAt(debug_index + 17));
-	//alert(res.charCodeAt(debug_index + 18));
-	//alert(res.charCodeAt(debug_index + 19));
+		////alert(res.charCodeAt (0));
+		////alert(res.charCodeAt (1));
+		////alert(res.charCodeAt (2));
+		//var debug_index = res.indexOf ('</ResponseWrapper>');
+		////alert(res.charCodeAt(debug_index + 17));
+		////alert(res.charCodeAt(debug_index + 18));
+		////alert(res.charCodeAt(debug_index + 19));
 
-	// if charset is x-user-defined split by \uF7FF
-	// if charset is utf-8, split by FFFD
-	// And dont ask me why!
-	var responses = res.split('\uFFFD'); 
-	var parser = new DOMParser();
+		// if charset is x-user-defined split by \uF7FF
+		// if charset is utf-8, split by FFFD
+		// And dont ask me why!
+		var responses = res.split('\uFFFD'); 
+		var parser = new DOMParser();
 
-//	Appending without clearing is bad... mmkay?
-//	document.getElementById('status').innerHTML='';
-	document.getElementById('results').innerHTML='';
-	document.getElementById('timetaken').innerHTML=(elapsed + ' seconds');
-	document.getElementById('numhits').innerHTML=0;
-	var num_matches = 0;
+		// Appending without clearing is bad... mmkay?
+		document.getElementById('results').innerHTML = '';
+		document.getElementById('timetaken').innerHTML = (elapsed + ' seconds');
+		document.getElementById('numhits').innerHTML = 0;
+		var num_matches = 0;
 
-	for (var i=0; i<responses.length; ++i) {
-	    if (responses[i].length <= 0)
-		continue;
-	    dump(responses[i]);
-	    dump('\n');
+		// Create blank xml document
+		var doc = document.implementation.createDocument('', '', null);
+		// Hack to get the xml declaration (https://bugzilla.mozilla.org/show_bug.cgi?id=318086)
+		var xml_decl = doc.createProcessingInstruction('xml', 'version="1.0" encoding="utf-8"'); 
 
-	    //document.getElementById('xml').innerHTML += (responses[i].length + "</br>");
-	    var response_dom = parser.parseFromString(responses[i], "text/xml");
+		// Create element <ResponseWrapper ...>
+		var res_wrapper = doc.createElement('ResponseWrapper');
+		res_wrapper.setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+		res_wrapper.setAttribute('xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
 
-	    var num_matches_elems = response_dom.getElementsByTagName('NumMatches');
-	    if (num_matches_elems.length > 0) {
-		var n = parseInt (num_matches_elems[0].textContent);
-		if (n > 0) {
-		    num_matches += n;
-		    document.getElementById('numhits').innerHTML=num_matches;
+		// Create element <Message ...>
+		var msg_elem = doc.createElement('Message');
+		msg_elem.setAttributeNS('http://www.w3.org/2001/XMLSchema-instance', 'xsi:type', 'HitsAddedResponse');
+
+		// Create element <Hits>
+		var hits_elem = doc.createElement('Hits');
+
+		// XXX: Process xml nodes before merging and sending to xsl
+		for (var i = 0; i < responses.length; ++i) {
+			if (responses[i].length <= 0)
+				continue;
+
+			var response_dom = parser.parseFromString(responses[i], "text/xml");
+			// FIXME: ignoring all other messages
+			hits = response_dom.getElementsByTagName('Hit');
+			for (var j = 0; j < hits.length; ++j) {
+				// Clone every <Hit> (and its children) and append inside <Hits>
+				hits_elem.appendChild(hits[j].cloneNode(true));
+			}
+
+			var num_matches_elems = response_dom.getElementsByTagName('NumMatches');
+			if (num_matches_elems.length > 0) {
+				var n = parseInt (num_matches_elems[0].textContent);
+				if (n > 0) {
+					num_matches += n;
+					document.getElementById('numhits').innerHTML=num_matches;
+				}
+			}
 		}
-	    }
-	    //var hits = response_dom.getElementsByTagName('Hit');
-	    //if (hits.length > 0) {
-	    //    document.getElementById('results').innerHTML = ("<i>" + hits[0].getAttribute('Timestamp') + "</i>");
-	    //    document.getElementById('xml').innerHTML = ("<i>" + hits[1].getAttribute('Timestamp') + "</i>");
-	    //}
-	    var fragment = xsltProcessor.transformToFragment(response_dom, document);
-	    document.getElementById('results').appendChild(fragment);
+
+		// Append <Hits> inside <Message ...>
+		msg_elem.appendChild(hits_elem);
+		// Append <Message ...> inside <ResponseWrapper ...>
+		res_wrapper.appendChild(msg_elem);
+		// Append everything inside the blank xml doc
+		doc.appendChild(res_wrapper);
+		doc.insertBefore(xml_decl, res_wrapper);
+
+		// Send	xml doc to xsl for processing
+		var result = xsltProcessor.transformToFragment(doc, document);
+		// Append resultant html inside <div id="results">
+		document.getElementById('results').appendChild(result);
+		// If no results..
+		if (document.getElementById('results').textContent == '') {
+			var no_results = document.createElement('p');
+			no_results.appendChild(document.createTextNode('No Results'));
+			document.getElementById('results').appendChild(no_results);
+		}
+		document.getElementById('topbar').style.display='block';
+		document.getElementById('status').style.display='none';
 	}
 
-	document.getElementById('topbar-right').style.display='block';
-	document.getElementById('status').style.display='none';
-    }
-
-    document.queryform.querytext.disabled=false;
+	document.queryform.querytext.disabled=false;
 }
 
 function state_change_info()
 {
-    //alert("help");
-    if (xmlhttp.readyState==4) {
-	// FIXME: Should also check for status 200
+	////alert("help");
+	if (xmlhttp.readyState==4) {
+		// FIXME: Should also check for status 200
 
-	//dump("Response:\n");
-	//dump(xmlhttp.responseText);
-	//dump("\n");
+		//dump("Response:\n");
+		//dump(xmlhttp.responseText);
+		//dump("\n");
 
-	res = xmlhttp.responseText;
+		res = xmlhttp.responseText;
 
-	// if charset is x-user-defined split by \uF7FF
-	// if charset is utf-8, split by FFFD
-	// And dont ask me why!
-	var responses = res.split('\uFFFD'); 
-	var parser = new DOMParser();
+		// if charset is x-user-defined split by \uF7FF
+		// if charset is utf-8, split by FFFD
+		// And dont ask me why!
+		var responses = res.split('\uFFFD'); 
+		var parser = new DOMParser();
 
-//	Appending without clearing is bad... mmkay?
-	document.getElementById('results').innerHTML='';
-	document.getElementById('topbar-right').style.display='none';
+//		Appending without clearing is bad... mmkay?
+		document.getElementById('results').innerHTML='';
+		document.getElementById('topbar').style.display='none';
 
-	// there should be only one response in responses
-	for (var i=0; i<responses.length; ++i) {
-	    if (responses[i].length <= 0)
-		continue;
-	    dump(responses[i]);
-	    dump('\n');
+		// there should be only one response in responses
+		for (var i=0; i<responses.length; ++i) {
+			if (responses[i].length <= 0)
+				continue;
+			dump(responses[i]);
+			dump('\n');
 
-	    var response_dom = parser.parseFromString(responses[i], "text/xml");
-	    var fragment = xsltProcessor.transformToFragment(response_dom, document);
-	    document.getElementById('results').appendChild(fragment);
+			var response_dom = parser.parseFromString(responses[i], "text/xml");
+			var fragment = xsltProcessor.transformToFragment(response_dom, document);
+			document.getElementById('results').appendChild(fragment);
+		}
+
+		document.getElementById('status').style.display='none';
 	}
 
-	document.getElementById('status').style.display='none';
-    }
-
-    document.queryform.querytext.disabled=false;
+	document.queryform.querytext.disabled=false;
 }
 
+/* Start Fancy Stuff */
+/* This part shows/hides results
+ * TODO:
+ * - "Remember" choices between searches
+ * - Categories.. opinions?
+ */
 
+function toggle_hit(hit_toggle) {
+	if (hit_toggle.innerHTML == '[+]') {
+		hit_toggle.innerHTML = '[-]';
+		//this.<span class="Uri">.<div class="Title">.<br/>.<div class="Data">
+		hit_toggle.parentNode.parentNode.nextSibling.nextSibling.style.display = 'none';
+
+	} else {
+		hit_toggle.innerHTML = '[+]';
+		hit_toggle.parentNode.parentNode.nextSibling.nextSibling.style.display = 'block';
+	}
+}
+
+function toggle_category(category) {
+	// Get all the category checkboxes, global variable
+	category_checkboxes = document.getElementById('topbar-left').getElementsByTagName('input');
+	// Get all the result categories, global variable
+	result_categories = document.getElementById('results').getElementsByTagName('div');
+	// If "All" is clicked..
+	if (category.name == "All") {
+		// If checked right now, it was unchecked earlier
+		if (category.checked) {
+			// Show all results
+			document.getElementById('results').style.display= 'block';
+			for (var i = 0; i < result_categories.length; ++i) {
+				result_categories[i].style.display = 'block';
+			}
+			// Check all the categories
+			for (var i = 0; i < category_checkboxes.length; ++i) {
+				category_checkboxes[i].checked = true;
+			}
+		} else {
+			// Hide all results
+			document.getElementById('results').style.display= 'none';
+			// Uncheck all the categories
+			for (var i = 0; i < category_checkboxes.length; ++i) {
+				category_checkboxes[i].checked = false;
+			}
+		}
+
+	// If some other checkbox is clicked,
+	} else {
+		// if checked right now
+		if (category.checked) {
+			// Show result corresponding to category
+			document.getElementById(category.name).style.display = 'block';
+		} else {
+			// Hide result corresponding to category
+			document.getElementById(category.name).style.display = 'none';
+		}
+
+	}
+}
--- beagle/beagled/webinterface/queryresult.xsl
+++ beagle/beagled/webinterface/queryresult.xsl
@@ -4,86 +4,242 @@
 <xsl:output method="html" />
 
 <xsl:template match="/">
-    <!--<html><body>-->
-    <xsl:apply-templates select="ResponseWrapper"/>
-    <!--</body></html>-->
+	<!--<html><body>-->
+	<xsl:apply-templates select="ResponseWrapper"/>
+	<!--</body></html>-->
 </xsl:template>
 
 <xsl:template match="ResponseWrapper">
-    <xsl:apply-templates select="Message[@xsi:type = 'HitsAddedResponse']"/>
-    <xsl:apply-templates select="Message[@xsi:type = 'DaemonInformationResponse']"/>
+	<xsl:apply-templates select="Message[@xsi:type = 'HitsAddedResponse']"/>
+	<xsl:apply-templates select="Message[@xsi:type = 'DaemonInformationResponse']"/>
 </xsl:template>
 
 <xsl:template match="Message[@xsi:type = 'HitsAddedResponse']">
-    <!--<xsl:apply-templates select="NumMatches"/>-->
-    <xsl:apply-templates select="Hits"/>
-    <!-- FIXME: Ignoring other types of messages -->
+	<!--<xsl:apply-templates select="NumMatches"/>-->
+	<xsl:apply-templates select="Hits"/>
+	<!-- FIXME: Ignoring other types of messages -->
 </xsl:template>
 
 <xsl:template match="Message[@xsi:type = 'DaemonInformationResponse']">
-    <div id="version"><b>Version</b>: <i><xsl:value-of select="Version"/></i></div>
-    <div id="is_indexing"><b>Indexing in progress</b>: <i><xsl:value-of select="IsIndexing"/></i></div>
-    <xsl:apply-templates select="SchedulerInformation"/>
-    <xsl:apply-templates select="IndexStatus"/>
+	<div id="version"><b>Version</b>: <i><xsl:value-of select="Version"/></i></div>
+	<div id="is_indexing"><b>Indexing in progress</b>: <i><xsl:value-of select="IsIndexing"/></i></div>
+	<xsl:apply-templates select="SchedulerInformation"/>
+	<xsl:apply-templates select="IndexStatus"/>
 </xsl:template>
 
 <xsl:template match="SchedulerInformation">
-    <div class="scheduler_information"><b>Tasks:</b><br/>
-	<span class="total_task_count">(<xsl:value-of select="@TotalTaskCount"/> tasks submitted)</span>
-	<ul>
-	<xsl:for-each select="PendingTasks/PendingTask">
-	    <li><i>(Pending)</i>&nbsp;<xsl:value-of select="."/></li>
-	</xsl:for-each>
+	<div class="scheduler_information"><b>Tasks:</b><br/>
+		<span class="total_task_count">(<xsl:value-of select="@TotalTaskCount"/> tasks submitted)</span>
+		<ul>
+		<xsl:for-each select="PendingTasks/PendingTask">
+			<li><i>(Pending)</i>&nbsp;<xsl:value-of select="."/></li>
+		</xsl:for-each>
 
-	<xsl:for-each select="FutureTasks/FutureTask">
-	    <li><i>(Future)</i>&nbsp;<xsl:value-of select="."/></li>
-	</xsl:for-each>
+		<xsl:for-each select="FutureTasks/FutureTask">
+			<li><i>(Future)</i>&nbsp;<xsl:value-of select="."/></li>
+		</xsl:for-each>
 
-	<xsl:for-each select="BlockedTasks/BlockedTask">
-	    <li><i>(Blocked)</i>&nbsp;<xsl:value-of select="."/></li>
-	</xsl:for-each>
-	</ul>
-    </div>
+		<xsl:for-each select="BlockedTasks/BlockedTask">
+			<li><i>(Blocked)</i>&nbsp;<xsl:value-of select="."/></li>
+		</xsl:for-each>
+		</ul>
+	</div>
 </xsl:template>
 
 <xsl:template match="IndexStatus">
-    <b>Details of backends:</b><br/>
-    <ul class="indexstatus">
-	<xsl:for-each select="QueryableStatus">
-	    <li><div class="queryablestatus">
-		<span class="queryablestatus_name"><xsl:value-of select="@Name"/></span>&nbsp;
-		<span class="queryablestatus_progresspercent">(<xsl:value-of select="@ProgressPercent"/> %)</span>:
-		<span class="queryablestatus_itemcount"><xsl:value-of select="@ItemCount"/> items,</span>
-		<span class="queryablestatus_isindexing">currently indexing? <xsl:value-of select="@IsIndexing"/></span>,
-	    </div></li>
-	</xsl:for-each>
-    </ul>
+	<b>Details of backends:</b><br/>
+	<ul class="indexstatus">
+		<xsl:for-each select="QueryableStatus">
+			<li><div class="queryablestatus">
+			<span class="queryablestatus_name"><xsl:value-of select="@Name"/></span>&nbsp;
+			<span class="queryablestatus_progresspercent">(<xsl:value-of select="@ProgressPercent"/> %)</span>:
+			<span class="queryablestatus_itemcount"><xsl:value-of select="@ItemCount"/> items,</span>
+			<span class="queryablestatus_isindexing">currently indexing? <xsl:value-of select="@IsIndexing"/></span>,
+			</div></li>
+		</xsl:for-each>
+	</ul>
+</xsl:template>
+
+<!-- FileType: document,directory,mail,image,audio,video,archive,package -->
+<xsl:template match="Hits">
+	<xsl:call-template name="DocumentHits"/>
+	<xsl:call-template name="ImageHits"/>
+	<xsl:call-template name="MediaHits"/>
+	<xsl:call-template name="MailHits"/>
+	<xsl:call-template name="IMLogHits"/>
+	<xsl:call-template name="WebsiteHits"/>
+	<xsl:call-template name="OtherHits"/>
+	<!--
+	<xsl:call-template name="DocumentHits"/>
+	-->
+</xsl:template>
+
+
+<xsl:template match="NumMatches">
+	<p class="NumMatches">(Showing <xsl:value-of select="text()"/> matches)</p>
+</xsl:template>
+
+<xsl:template match="@Timestamp">
+	<span class="Timestamp">
+		Last modified: <b><xsl:value-of select="../@Timestamp"/></b>
+	</span>
+</xsl:template>
+
+<xsl:template match="Hit">
+	 <div class="Hit" id="{@Uri}">
+		<div class="Title">
+			<span class="Uri">
+				<a href="#" class="Toggle" onclick='toggle_hit(this); return false;'>[+]</a>
+				<a href="{@Uri}">
+				<xsl:choose>
+				<xsl:when test="Properties/Property[@Key='beagle:ExactFilename']">
+					<xsl:value-of select="Properties/Property[@Key='beagle:ExactFilename']/@Value"/>
+				</xsl:when>
+				<xsl:when test="Properties/Property[@Key='dc:title']">
+					<xsl:value-of select="Properties/Property[@Key='dc:title']/@Value"/>
+				</xsl:when>
+				<xsl:when test="Properties/Property[@Key='parent:dc:title']">
+					<xsl:value-of select="Properties/Property[@Key='parent:dc:title']/@Value"/>
+				</xsl:when>
+				<xsl:when test="Properties/Property[@Key='beagle:HitType' and @Value='IMLog']">
+					Conversation with&nbsp;
+					<xsl:choose>
+						<xsl:when test="Properties/Property[@Key='fixme:alias']">
+							<xsl:value-of select="Properties/Property[@Key='fixme:alias']/@Value"/>
+						</xsl:when>
+						<xsl:otherwise>
+							<xsl:value-of select="Properties/Property[@Key='fixme:speakingto']/@Value"/>
+						</xsl:otherwise>
+					</xsl:choose>
+				</xsl:when>
+				<xsl:otherwise>
+					<xsl:value-of select="@Uri"/>
+				</xsl:otherwise>
+				</xsl:choose>
+				</a>
+			</span>
+			<xsl:apply-templates select="@Timestamp"/>
+		</div><br/>
+		<div class="Data">
+			<xsl:apply-templates select="Properties"/>
+		</div>
+	</div>
 </xsl:template>
 
 <xsl:template match="NumMatches">
-    <p class="NumMatches">(Showing <xsl:value-of select="text()"/> matches)</p>
+	<p class="NumMatches">(Showing <xsl:value-of select="text()"/> matches)</p>
 </xsl:template>
 
-<xsl:template match="Hits">
-	<xsl:for-each select="Hit">
-	    <div class="Hit" id="{@Uri}">
-		<div class="Uri"><a href="{@Uri}"><xsl:value-of select="@Uri"/></a></div><br/>
-		<span class="Timestamp">Timestamp:<i><xsl:value-of select="@Timestamp"/></i><b>|</b></span>
-		<span class="Score">Score:<i><xsl:value-of select="@Score"/></i></span>
-	    <xsl:apply-templates select="Properties"/>
-	    </div>
-	</xsl:for-each>
+<xsl:template match="@Timestamp">
+	<span class="Timestamp">
+		Last modified: <b><xsl:value-of select="../@Timestamp"/></b>
+	</span>
+</xsl:template>
+
+<!-- 
+Includes:
+ - FileType = {document,archive,source}
+ - HitType != {WebHistory,MailMessage}
+-->
+<xsl:template name="DocumentHits">
+	<div id="Documents">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:FileType' and (@Value = 'document' or @Value = 'archive' or @Value = 'source')] and Properties/Property[@Key = 'beagle:HitType' and (@Value != 'WebHistory' and @Value != 'MailMessage')]]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+
+<!-- Merged into 'DocumentHits' -->
+<!--
+<xsl:template name="FolderHits">
+	<div id="Folders">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:FileType' and @Value = 'directory']]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+-->
+
+<xsl:template name="ImageHits">
+	<div id="Images">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:FileType' and @Value = 'image']]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+
+<xsl:template name="MailHits">
+	<div id="Mail">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:HitType' and @Value = 'MailMessage']]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+
+<xsl:template name="MediaHits">
+	<div id="Media">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:FileType' and (@Value = 'audio' or @Value = 'video')]]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+
+<!-- Merged into 'MediaHits' -->
+<!--
+<xsl:template name="VideoHits">
+	<div id="Video">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:FileType' and @Value = 'video']]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+-->
+
+<xsl:template name="IMLogHits">
+	<div id="IMLogs">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:HitType' and @Value = 'IMLog']]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+
+<xsl:template name="WebsiteHits">
+	<div id="Websites">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:HitType' and (@Value = 'WebHistory' or @Value = 'FirefoxBookmark')]]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+
+<!-- Merged into 'DocumentHits' -->
+<!--
+<xsl:template name="SourceHits">
+	<div id="Source">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:FileType' and @Value = 'source']]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
+</xsl:template>
+-->
+
+<!-- Everything else goes here. So, fill 'er up :) -->
+<xsl:template name="OtherHits">
+	<div id="Others">
+		<xsl:for-each select="Hit[Properties/Property[@Key = 'beagle:FileType' and (@Value = 'directory')]]">
+			<xsl:apply-templates select="."/>
+		</xsl:for-each>
+	</div>
 </xsl:template>
 
 <xsl:template match="Properties">
-    <table class="Properties">
-	<xsl:for-each select="Property">
-	    <tr>
-	    <td class="PropertyKey"><xsl:value-of select="@Key"/></td>
-	    <td class="PropertyValue"><xsl:value-of select="@Value"/></td>
-	    </tr>
-	</xsl:for-each>
-    </table>
+	<table class="Properties">
+		<xsl:for-each select="Property">
+			<tr>
+			<td class="PropertyKey"><xsl:value-of select="@Key"/></td>
+			<td class="PropertyValue"><xsl:value-of select="@Value"/></td>
+			</tr>
+		</xsl:for-each>
+	</table>
 </xsl:template>
 
 </xsl:stylesheet>
