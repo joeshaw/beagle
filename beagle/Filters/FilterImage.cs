joeshaw/beagle

commit 6710bab0c8e35ed2f6ebf7064c066e16da1602d8

Build fixes. * Build with gmime-2.4 - patch from Jeffrey Stedfast * Build with newer Mono.Data.Sqlite 2.x - patch from Gabriel Burt

beagle/Util/FSpotTools.cs
@@ -86,7 +86,7 @@ namespace Beagle.Util {
 								try {
 									reader = command.ExecuteReader ();
 								} catch (SqliteException e) {
-									if (e.ErrorCode == SQLiteErrorCode.Busy) {
+									if (e.ErrorCode == SqliteErrorCode.Busy) {
 										Thread.Sleep (50);
 									} else {
 										throw;

beagle/beagled/SqliteUtils.cs
@@ -56,7 +56,7 @@ namespace Beagle.Util {
 						ret = command.ExecuteNonQuery ();
 						break;
 					} catch (SqliteException ex) {
-						if (ex.ErrorCode == SQLiteErrorCode.Busy) {
+						if (ex.ErrorCode == SqliteErrorCode.Busy) {
 							Thread.Sleep (50);
 						} else {
 							Log.Error (ex, "SQL that caused the exception: {0}", command_text);
@@ -85,7 +85,7 @@ namespace Beagle.Util {
 					ret = command.ExecuteNonQuery ();
 					break;
 				} catch (SqliteException ex) {
-					if (ex.ErrorCode == SQLiteErrorCode.Busy) {
+					if (ex.ErrorCode == SqliteErrorCode.Busy) {
 						Thread.Sleep (50);
 					} else {
 						Log.Error (ex, "SQL that caused the exception: {0}", command.CommandText);
@@ -107,7 +107,7 @@ namespace Beagle.Util {
 				try {
 					reader = command.ExecuteReader ();
 				} catch (SqliteException ex) {
-					if (ex.ErrorCode == SQLiteErrorCode.Busy) {
+					if (ex.ErrorCode == SqliteErrorCode.Busy) {
 						Thread.Sleep (50);
 					} else {
 						throw;
@@ -123,7 +123,7 @@ namespace Beagle.Util {
 				try {
 					return reader.Read ();
 				} catch (SqliteException ex) {
-					if (ex.ErrorCode == SQLiteErrorCode.Busy) {
+					if (ex.ErrorCode == SqliteErrorCode.Busy) {
 						Thread.Sleep (50);
 					} else {
 						throw;

beagle/configure.in
@@ -17,7 +17,7 @@ MONODOC_REQUIRED=1.2.4
 NDESK_DBUS_REQUIRED=0.5.2
 NDESK_DBUS_GLIB_REQUIRED=0.3.0
 GTK_SHARP_REQUIRED=2.10.0
-GMIME_SHARP_REQUIRED=2.2.0
+GMIME_SHARP_REQUIRED=2.3.5
 EVOLUTION_SHARP_REQUIRED=0.13.3
 GSF_SHARP_REQUIRED=0.6
 GTK_REQUIRED=2.10.0
@@ -223,7 +223,7 @@ if test "x$enable_gui" = "xyes"; then
 	glade-sharp-2.0 >= $GTK_SHARP_REQUIRED
 	gnome-sharp-2.0 >= $GTK_SHARP_REQUIRED
 	gnome-vfs-sharp-2.0 >= $GTK_SHARP_REQUIRED
-	gmime-sharp >= $GMIME_SHARP_REQUIRED
+	gmime-sharp-2.4 >= $GMIME_SHARP_REQUIRED
 ])
 	AC_SUBST(BEAGLE_UI_LIBS)
 
@@ -270,7 +270,7 @@ PKG_CHECK_MODULES(EVO,
                   evolution-sharp >= $EVOLUTION_SHARP_REQUIRED 	\
                   gconf-sharp-2.0 >= $GTK_SHARP_REQUIRED	\
 		  glib-sharp-2.0 >= $GTK_SHARP_REQUIRED
-                  gmime-sharp >= $GMIME_SHARP_REQUIRED,
+                  gmime-sharp-2.4 >= $GMIME_SHARP_REQUIRED,
                   have_evo_dependencies=yes, have_evo_dependencies=no)
 AC_SUBST(EVO_LIBS)
 
@@ -377,14 +377,14 @@ dnl ----------------------------------------------
 PKG_CHECK_MODULES(BEAGLED,
 [
 	shared-mime-info
-	gmime-sharp >= $GMIME_SHARP_REQUIRED
+	gmime-sharp-2.4 >= $GMIME_SHARP_REQUIRED
 	glib-sharp-2.0 >= $GTK_SHARP_REQUIRED
 ])
 BEAGLED_LIBS="$BEAGLED_LIBS $GSF_SHARP_LIBS"
 AC_SUBST(BEAGLED_LIBS)
 
 GSF_SHARP_PREFIX=`$PKG_CONFIG --variable=prefix gsf-sharp`
-GMIME_SHARP_PREFIX=`$PKG_CONFIG --variable=prefix gmime-sharp`
+GMIME_SHARP_PREFIX=`$PKG_CONFIG --variable=prefix gmime-sharp-2.4`
 
 dnl ----------------------------------------------
 dnl Epiphany Extension

beagle/Filters/FilterMail.cs
@@ -131,7 +131,7 @@ namespace Beagle.Filters {
 
 			// Messages that are multipart/alternative shouldn't be considered as having
 			// attachments.  Unless of course they do.
-			if (mime_part is GMime.Multipart && mime_part.ContentType.Subtype.ToLower () != "alternative")
+			if (mime_part is GMime.Multipart && mime_part.ContentType.MediaSubtype.ToLower () != "alternative")
 				return true;
 
 			return false;
@@ -145,34 +145,43 @@ namespace Beagle.Filters {
 			AddProperty (Property.NewDate ("fixme:date", message.Date.ToUniversalTime ()));
 
 			GMime.InternetAddressList addrs;
-			addrs = this.message.GetRecipients (GMime.Message.RecipientType.To);
+			addrs = this.message.GetRecipients (GMime.RecipientType.To);
 			foreach (GMime.InternetAddress ia in addrs) {
 				AddProperty (Property.NewUnsearched ("fixme:to", ia.ToString (false)));
-				if (ia.AddressType != GMime.InternetAddressType.Group)
-					AddProperty (Property.New ("fixme:to_address", ia.Addr));
-
+				if (ia is GMime.InternetAddressMailbox) {
+					GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+					
+					AddProperty (Property.New ("fixme:to_address", mailbox.Address));
+				}
+				
 				AddProperty (Property.New ("fixme:to_name", ia.Name));
 				AddEmailLink (ia);
 			}
 			addrs.Dispose ();
 
-			addrs = this.message.GetRecipients (GMime.Message.RecipientType.Cc);
+			addrs = this.message.GetRecipients (GMime.RecipientType.Cc);
 			foreach (GMime.InternetAddress ia in addrs) {
 				AddProperty (Property.NewUnsearched ("fixme:cc", ia.ToString (false)));
-				if (ia.AddressType != GMime.InternetAddressType.Group)
-					AddProperty (Property.New ("fixme:cc_address", ia.Addr));
-
+				if (ia is GMime.InternetAddressMailbox) {
+					GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+					
+					AddProperty (Property.New ("fixme:cc_address", mailbox.Address));
+				}
+				
 				AddProperty (Property.New ("fixme:cc_name", ia.Name));
 				AddEmailLink (ia);
 			}
 			addrs.Dispose ();
 
-			addrs = GMime.InternetAddressList.ParseString (GMime.Utils.HeaderDecodePhrase (this.message.Sender));
+			addrs = GMime.InternetAddressList.Parse (this.message.Sender);
 			foreach (GMime.InternetAddress ia in addrs) {
 				AddProperty (Property.NewUnsearched ("fixme:from", ia.ToString (false)));
-				if (ia.AddressType != GMime.InternetAddressType.Group)
-					AddProperty (Property.New ("fixme:from_address", ia.Addr));
-
+				if (ia is GMime.InternetAddressMailbox) {
+					GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+					
+					AddProperty (Property.New ("fixme:from_address", mailbox.Address));
+				}
+				
 				AddProperty (Property.New ("fixme:from_name", ia.Name));
 				AddEmailLink (ia);
 			}
@@ -189,7 +198,7 @@ namespace Beagle.Filters {
 				AddProperty (Property.NewUnsearched ("fixme:msgid", GMime.Utils.DecodeMessageId (msgid)));
 
 			foreach (GMime.References refs in this.message.References)
-				AddProperty (Property.NewUnsearched ("fixme:reference", refs.Msgid));
+				AddProperty (Property.NewUnsearched ("fixme:reference", refs.MessageId));
 
 			string list_id = this.message.GetHeader ("List-Id");
 			if (list_id != null)
@@ -350,16 +359,15 @@ namespace Beagle.Filters {
 					}
 				} else if (mime_part is GMime.Multipart) {
 					GMime.Multipart multipart = (GMime.Multipart) mime_part;
-
-					int num_parts = multipart.Number;
+					int num_parts = multipart.Count;
 
 					// If the mimetype is multipart/alternative, we only want to index
 					// one part -- the richest one we can filter.
-					if (mime_part.ContentType.Subtype.ToLower () == "alternative") {
+					if (mime_part.ContentType.MediaSubtype.ToLower () == "alternative") {
 						// The richest formats are at the end, so work from there
 						// backward.
 						for (int i = num_parts - 1; i >= 0; i--) {
-							GMime.Object subpart = multipart.GetPart (i);
+							GMime.Object subpart = multipart[i];
 
 							if (IsMimeTypeHandled (subpart.ContentType.ToString ())) {
 								part = subpart;
@@ -375,7 +383,7 @@ namespace Beagle.Filters {
 					// the parts, treat them like a bunch of attachments.
 					if (part == null) {
 						for (int i = 0; i < num_parts; i++) {
-							using (GMime.Object subpart = multipart.GetPart (i))
+							using (GMime.Object subpart = multipart[i])
 								this.OnEachPart (subpart);
 						}
 					}
@@ -405,7 +413,7 @@ namespace Beagle.Filters {
 						} else if (mime_type == "text/html") {
 							no_child_needed = true;
 							html_part = true;
-							string enc = part.GetContentTypeParameter ("charset"); 
+							string enc = part.ContentType.GetParameter ("charset"); 
 							// DataWrapper.Stream is a very limited stream
 							// and does not allow Seek or Tell
 							// HtmlFilter requires Stream.Position=0.
@@ -479,7 +487,7 @@ namespace Beagle.Filters {
 							if (length != -1)
 								child.AddProperty (Property.NewUnsearched ("fixme:filesize", length));
 
-							if (part.ContentType.Type.ToLower () == "text")
+							if (part.ContentType.MediaType.ToLower () == "text")
 								child.SetTextReader (new StreamReader (stream));
 							else
 								child.SetBinaryStream (stream);

beagle/beagled/EvolutionMailQueryable/EvolutionMailIndexableGenerator.cs
@@ -333,28 +333,46 @@ namespace Beagle.Daemon.EvolutionMailQueryable {
                         indexable.AddProperty (Property.NewUnsearched ("fixme:folder", this.folder_name));
 
 			GMime.InternetAddressList addrs;
-
-			addrs = message.GetRecipients (GMime.Message.RecipientType.To);
-			foreach (GMime.InternetAddress ia in addrs) {
-				if (this.folder_name == "Sent" && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", ia.Addr));
+			
+			if (this.folder_name == "Sent") {
+				addrs = message.GetRecipients (GMime.RecipientType.To);
+				foreach (GMime.InternetAddress ia in addrs) {
+					if (ia is GMime.InternetAddressMailbox) {
+						GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+						
+						indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", mailbox.Address));
+					}
+				}
+				
+				addrs.Dispose ();
 			}
-			addrs.Dispose ();
-
-			addrs = message.GetRecipients (GMime.Message.RecipientType.Cc);
-			foreach (GMime.InternetAddress ia in addrs) {
-				if (this.folder_name == "Sent"  && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", ia.Addr));
+			
+			if (this.folder_name == "Sent") {
+				addrs = message.GetRecipients (GMime.RecipientType.Cc);
+				foreach (GMime.InternetAddress ia in addrs) {
+					if (ia is GMime.InternetAddressMailbox) {
+						GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+						
+						indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", mailbox.Address));
+					}
+				}
+				
+				addrs.Dispose ();
 			}
-			addrs.Dispose ();
-
-			addrs = GMime.InternetAddressList.ParseString (GMime.Utils.HeaderDecodePhrase (message.Sender));
-			foreach (GMime.InternetAddress ia in addrs) {
-				if (this.folder_name != "Sent"  && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewUnsearched ("fixme:gotFrom", ia.Addr));
+			
+			if (this.folder_name != "Sent") {
+				addrs = GMime.InternetAddressList.Parse (message.Sender);
+				foreach (GMime.InternetAddress ia in addrs) {
+					if (ia is GMime.InternetAddressMailbox) {
+						GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+					
+						indexable.AddProperty (Property.NewUnsearched ("fixme:gotFrom", mailbox.Address));
+					}
+				}
+				
+				addrs.Dispose ();
 			}
-			addrs.Dispose ();
-
+			
 			if (this.folder_name == "Sent")
 				indexable.AddProperty (Property.NewFlag ("fixme:isSent"));
 
@@ -750,48 +768,54 @@ namespace Beagle.Daemon.EvolutionMailQueryable {
 			}
 
 			GMime.InternetAddressList addrs;
-			addrs = GMime.InternetAddressList.ParseString (messageInfo.to);
+			addrs = GMime.InternetAddressList.Parse (messageInfo.to);
 			foreach (GMime.InternetAddress ia in addrs) {
+				GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+				
 				if (!have_content) {
 					indexable.AddProperty (Property.NewUnsearched ("fixme:to", ia.ToString (false)));
-					if (ia.AddressType != GMime.InternetAddressType.Group)
-						indexable.AddProperty (Property.New ("fixme:to_address", ia.Addr));
-
+					if (ia is GMime.InternetAddressMailbox)
+						indexable.AddProperty (Property.New ("fixme:to_address", mailbox.Address));
+					
 					indexable.AddProperty (Property.New ("fixme:to_name", ia.Name));
 				}
-
-				if (this.folder_name == "Sent" && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", ia.Addr));
+				
+				if (this.folder_name == "Sent" && ia is GMime.InternetAddressMailbox)
+					indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", mailbox.Address));
 			}
 			addrs.Dispose ();
 
-			addrs = GMime.InternetAddressList.ParseString (messageInfo.cc);
+			addrs = GMime.InternetAddressList.Parse (messageInfo.cc);
 			foreach (GMime.InternetAddress ia in addrs) {
+				GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+				
 				if (!have_content) {
 					indexable.AddProperty (Property.NewUnsearched ("fixme:cc", ia.ToString (false)));
-					if (ia.AddressType != GMime.InternetAddressType.Group)
-						indexable.AddProperty (Property.New ("fixme:cc_address", ia.Addr));
-
+					if (ia is GMime.InternetAddressMailbox)
+						indexable.AddProperty (Property.New ("fixme:cc_address", mailbox.Address));
+					
 					indexable.AddProperty (Property.New ("fixme:cc_name", ia.Name));
 				}
-
-				if (this.folder_name == "Sent" && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", ia.Addr));
+				
+				if (this.folder_name == "Sent" && ia is GMime.InternetAddressMailbox)
+					indexable.AddProperty (Property.NewUnsearched ("fixme:sentTo", mailbox.Address));
 			}
 			addrs.Dispose ();
 
-			addrs = GMime.InternetAddressList.ParseString (messageInfo.from);
+			addrs = GMime.InternetAddressList.Parse (messageInfo.from);
 			foreach (GMime.InternetAddress ia in addrs) {
+				GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+				
 				if (!have_content) {
 					indexable.AddProperty (Property.NewUnsearched ("fixme:from", ia.ToString (false)));
-					if (ia.AddressType != GMime.InternetAddressType.Group)
-						indexable.AddProperty (Property.New ("fixme:from_address", ia.Addr));
-
+					if (ia is GMime.InternetAddressMailbox)
+						indexable.AddProperty (Property.New ("fixme:from_address", mailbox.Address));
+					
 					indexable.AddProperty (Property.New ("fixme:from_name", ia.Name));
 				}
 
-				if (this.folder_name != "Sent" && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewUnsearched ("fixme:gotFrom", ia.Addr));
+				if (this.folder_name != "Sent" && ia is GMime.InternetAddressMailbox)
+					indexable.AddProperty (Property.NewUnsearched ("fixme:gotFrom", mailbox.Address));
 			}
 			addrs.Dispose ();
 

beagle/beagled/GoogleBackends/GMailSearchDriver.cs
@@ -322,38 +322,47 @@ namespace Beagle.Daemon.GoogleBackend {
 			hit.AddProperty (Property.NewDate ("fixme:date", message.Date.ToUniversalTime ()));
 
 			GMime.InternetAddressList addrs;
-			addrs = message.GetRecipients (GMime.Message.RecipientType.To);
+			addrs = message.GetRecipients (GMime.RecipientType.To);
 			foreach (GMime.InternetAddress ia in addrs) {
 				hit.AddProperty (Property.NewUnsearched ("fixme:to", ia.ToString (false)));
-				if (ia.AddressType != GMime.InternetAddressType.Group)
-					hit.AddProperty (Property.New ("fixme:to_address", ia.Addr));
-
+				if (ia is GMime.InternetAddressMailbox) {
+					GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+					
+					hit.AddProperty (Property.New ("fixme:to_address", mailbox.Address));
+				}
+				
 				hit.AddProperty (Property.New ("fixme:to_name", ia.Name));
 			}
 			addrs.Dispose ();
 
-			addrs = message.GetRecipients (GMime.Message.RecipientType.Cc);
+			addrs = message.GetRecipients (GMime.RecipientType.Cc);
 			foreach (GMime.InternetAddress ia in addrs) {
 				hit.AddProperty (Property.NewUnsearched ("fixme:cc", ia.ToString (false)));
-				if (ia.AddressType != GMime.InternetAddressType.Group)
-					hit.AddProperty (Property.New ("fixme:cc_address", ia.Addr));
-
+				if (ia is GMime.InternetAddressMailbox) {
+					GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+					
+					hit.AddProperty (Property.New ("fixme:cc_address", mailbox.Address));
+				}
+				
 				hit.AddProperty (Property.New ("fixme:cc_name", ia.Name));
 			}
 			addrs.Dispose ();
 
-			addrs = GMime.InternetAddressList.ParseString (GMime.Utils.HeaderDecodePhrase (message.Sender));
+			addrs = GMime.InternetAddressList.Parse (message.Sender);
 			foreach (GMime.InternetAddress ia in addrs) {
 				hit.AddProperty (Property.NewUnsearched ("fixme:from", ia.ToString (false)));
-				if (ia.AddressType != GMime.InternetAddressType.Group)
-					hit.AddProperty (Property.New ("fixme:from_address", ia.Addr));
-
+				if (ia is GMime.InternetAddressMailbox) {
+					GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+					
+					hit.AddProperty (Property.New ("fixme:from_address", mailbox.Address));
+				}
+				
 				hit.AddProperty (Property.New ("fixme:from_name", ia.Name));
 			}
 			addrs.Dispose ();
 
 			foreach (GMime.References refs in message.References)
-				hit.AddProperty (Property.NewUnsearched ("fixme:reference", refs.Msgid));
+				hit.AddProperty (Property.NewUnsearched ("fixme:reference", refs.MessageId));
 
 			string list_id = message.GetHeader ("List-Id");
 			if (list_id != null)

beagle/beagled/KMailQueryable/KMailIndexer.cs
@@ -407,28 +407,46 @@ namespace Beagle.Daemon.KMailQueryable {
                         indexable.AddProperty (Property.NewUnsearched ("fixme:folder", folder_name));
 
 			GMime.InternetAddressList addrs;
-
-			addrs = message.GetRecipients (GMime.Message.RecipientType.To);
-			foreach (GMime.InternetAddress ia in addrs) {
-				if (folder_name == Queryable.SentMailFolderName && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewKeyword ("fixme:sentTo", ia.Addr));
+			
+			if (folder_name == Queryable.SentMailFolderName) {
+				addrs = message.GetRecipients (GMime.RecipientType.To);
+				foreach (GMime.InternetAddress ia in addrs) {
+					if (ia is GMime.InternetAddressMailbox) {
+						GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+						
+						indexable.AddProperty (Property.NewKeyword ("fixme:sentTo", mailbox.Address));
+					}
+				}
+				
+				addrs.Dispose ();
 			}
-			addrs.Dispose ();
-
-			addrs = message.GetRecipients (GMime.Message.RecipientType.Cc);
-			foreach (GMime.InternetAddress ia in addrs) {
-				if (folder_name == Queryable.SentMailFolderName && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewKeyword ("fixme:sentTo", ia.Addr));
+			
+			if (folder_name == Queryable.SentMailFolderName) {
+				addrs = message.GetRecipients (GMime.RecipientType.Cc);
+				foreach (GMime.InternetAddress ia in addrs) {
+					if (ia is GMime.InternetAddressMailbox) {
+						GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+						
+						indexable.AddProperty (Property.NewKeyword ("fixme:sentTo", mailbox.Address));
+					}
+				}
+				
+				addrs.Dispose ();
 			}
-			addrs.Dispose ();
-
-			addrs = GMime.InternetAddressList.ParseString (GMime.Utils.HeaderDecodePhrase (message.Sender));
-			foreach (GMime.InternetAddress ia in addrs) {
-				if (folder_name != Queryable.SentMailFolderName && ia.AddressType != GMime.InternetAddressType.Group)
-					indexable.AddProperty (Property.NewKeyword ("fixme:gotFrom", ia.Addr));
+			
+			if (folder_name != Queryable.SentMailFolderName) {
+				addrs = GMime.InternetAddressList.Parse (message.Sender);
+				foreach (GMime.InternetAddress ia in addrs) {
+					if (ia is GMime.InternetAddressMailbox) {
+						GMime.InternetAddressMailbox mailbox = ia as GMime.InternetAddressMailbox;
+						
+						indexable.AddProperty (Property.NewKeyword ("fixme:gotFrom", mailbox.Address));
+					}
+				}
+				
+				addrs.Dispose ();
 			}
-			addrs.Dispose ();
-
+			
 			if (folder_name == Queryable.SentMailFolderName)
 				indexable.AddProperty (Property.NewFlag ("fixme:isSent"));
 			else {

commit 43e1ecdf231c36edd10381559ede9ea21e534ea6

Enable building with mono-2.8 (#632282)
1. mono-2.8 uses mono-2.pc instead of mono.pc for setting cflags and
libs; need to modify configure.in accordingly.
2. mono-2.8 disables direct access to struct MonoType fields; use
accessors instead (mono_type_get_type and mono_field_get_flags have
been present in the API since at least mono-1.2, so we do not need any
version ifdefs).
3. Cast FSpot.MetadataStore to IEnumerable in foreach() to prevent
CS1640 errors (non-unique enumeration of the type) with mono-2.8's gmcs.
4. Fix incorrect capitalization of SQLiteErrorCode introduced in commit
6710bab0c8e35ed2f6ebf7064c066e16da1602d8; for verification, see
http://github.com/mono/mono/blob/master/mcs/class/Mono.Data.Sqlite/Mono.Data.Sqlite_2.0/SQLiteException.cs

Signed-off-by: Alexandre Rostovtsev <tetromino@gmail.com>

beagle/Filters/FilterImage.cs
@@ -141,7 +141,7 @@ namespace Beagle.Filters {
 			Resource rights_anon = null;
 			Resource title_anon = null;
 
-			foreach (Statement stmt in xmp.Store) {
+			foreach (Statement stmt in (IEnumerable)xmp.Store) {
 				if (stmt.Predicate == MetadataStore.Namespaces.Resolve ("dc:subject")) {
 					//Console.WriteLine ("found subject");
 					subject_anon = stmt.Object;
@@ -163,7 +163,7 @@ namespace Beagle.Filters {
 				}
 			}
 			
-			foreach (Statement stmt in xmp.Store) {
+			foreach (Statement stmt in (IEnumerable)xmp.Store) {
 				if (stmt.Subject == subject_anon && 
 				    stmt.Predicate != MetadataStore.Namespaces.Resolve ("rdf:type")) {
 					AddProperty (Beagle.Property.New ("dc:subject", ((Literal)stmt.Object).Value));

beagle/Filters/FilterPdf.cs
@@ -8,6 +8,7 @@
 //
 
 using System;
+using System.Collections;
 using System.IO;
 using System.Diagnostics;
 
@@ -220,7 +221,7 @@ namespace Beagle.Filters {
 			Resource rights_anon = null;
 			Resource title_anon = null;
 
-			foreach (Statement stmt in xmp.Store) {
+			foreach (Statement stmt in (IEnumerable)xmp.Store) {
 				if (stmt.Predicate == MetadataStore.Namespaces.Resolve ("dc:subject")) {
 					//Console.WriteLine ("found subject");
 					subject_anon = stmt.Object;
@@ -239,7 +240,7 @@ namespace Beagle.Filters {
 				}
 			}
 			
-			foreach (Statement stmt in xmp.Store) {
+			foreach (Statement stmt in (IEnumerable)xmp.Store) {
 				if (stmt.Subject == subject_anon && 
 				    stmt.Predicate != MetadataStore.Namespaces.Resolve ("rdf:type")) {
 					AddProperty (Beagle.Property.New ("dc:subject", ((Literal)stmt.Object).Value));

beagle/Util/FSpotTools.cs
@@ -86,7 +86,7 @@ namespace Beagle.Util {
 								try {
 									reader = command.ExecuteReader ();
 								} catch (SqliteException e) {
-									if (e.ErrorCode == SqliteErrorCode.Busy) {
+									if (e.ErrorCode == SQLiteErrorCode.Busy) {
 										Thread.Sleep (50);
 									} else {
 										throw;

beagle/beagled/SqliteUtils.cs
@@ -56,7 +56,7 @@ namespace Beagle.Util {
 						ret = command.ExecuteNonQuery ();
 						break;
 					} catch (SqliteException ex) {
-						if (ex.ErrorCode == SqliteErrorCode.Busy) {
+						if (ex.ErrorCode == SQLiteErrorCode.Busy) {
 							Thread.Sleep (50);
 						} else {
 							Log.Error (ex, "SQL that caused the exception: {0}", command_text);
@@ -85,7 +85,7 @@ namespace Beagle.Util {
 					ret = command.ExecuteNonQuery ();
 					break;
 				} catch (SqliteException ex) {
-					if (ex.ErrorCode == SqliteErrorCode.Busy) {
+					if (ex.ErrorCode == SQLiteErrorCode.Busy) {
 						Thread.Sleep (50);
 					} else {
 						Log.Error (ex, "SQL that caused the exception: {0}", command.CommandText);
@@ -107,7 +107,7 @@ namespace Beagle.Util {
 				try {
 					reader = command.ExecuteReader ();
 				} catch (SqliteException ex) {
-					if (ex.ErrorCode == SqliteErrorCode.Busy) {
+					if (ex.ErrorCode == SQLiteErrorCode.Busy) {
 						Thread.Sleep (50);
 					} else {
 						throw;
@@ -123,7 +123,7 @@ namespace Beagle.Util {
 				try {
 					return reader.Read ();
 				} catch (SqliteException ex) {
-					if (ex.ErrorCode == SqliteErrorCode.Busy) {
+					if (ex.ErrorCode == SQLiteErrorCode.Busy) {
 						Thread.Sleep (50);
 					} else {
 						throw;

beagle/configure.in
@@ -59,11 +59,20 @@ if test "x$MCS" = "xno"; then
 	AC_MSG_ERROR([You need to install the Mono gmcs compiler])
 fi
 
-AC_MSG_CHECKING([for mono.pc])
-if test -z `$PKG_CONFIG --variable=prefix mono`; then
-  AC_MSG_ERROR([missing the mono.pc file, usually found in the mono-devel package])
+# mono-2.8 and higher uses mono-2.pc instead of mono.pc for cflags and libs
+AC_MSG_CHECKING([for mono-2.pc])
+if test -z `$PKG_CONFIG --variable=prefix mono-2`; then
+  AC_MSG_RESULT([not found])
+  AC_MSG_CHECKING([for mono.pc])
+  if test -z `$PKG_CONFIG --variable=prefix mono`; then
+    AC_MSG_ERROR([missing the mono.pc file, usually found in the mono-devel package])
+  else
+    AC_MSG_RESULT([found])
+    mono_pc=mono
+  fi
 else
   AC_MSG_RESULT([found])
+  mono_pc=mono-2
 fi
 
 BEAGLE_DEFINES=""
@@ -71,10 +80,10 @@ BEAGLE_DEFINES=""
 # check that we have the require version of mono
 
 # Temporary: check for mono-1.9
-PKG_CHECK_MODULES(MONO, mono >= 1.9, mono_1_9=yes, mono_1_9=no) 
+PKG_CHECK_MODULES(MONO, $mono_pc >= 1.9, mono_1_9=yes, mono_1_9=no)
 if test "x$mono_1_9" = "xno"; then
 	AC_MSG_RESULT([missing mono >= 1.9. Searching for mono >= 1.2.4])
-	PKG_CHECK_MODULES(MONO, mono >= $MONO_REQUIRED) 
+	PKG_CHECK_MODULES(MONO, $mono_pc >= $MONO_REQUIRED)
 else
 	AC_MSG_RESULT([found mono >= 1.9])
 	BEAGLE_DEFINES="$BEAGLE_DEFINES -define:MONO_1_9"
@@ -84,7 +93,7 @@ fi
 needed_dlls="Mono.Data.Sqlite Mono.Posix System.Data System.Web ICSharpCode.SharpZipLib"
 for i in $needed_dlls; do
   AC_MSG_CHECKING([for $i.dll])
-  if test ! -e `$PKG_CONFIG --variable=prefix mono`/lib/mono/2.0/$i.dll; then
+  if test ! -e `$PKG_CONFIG --variable=prefix $mono_pc`/lib/mono/2.0/$i.dll; then
     AC_MSG_ERROR([missing required mono DLL: $i.dll])
   else
     AC_MSG_RESULT([found])
@@ -195,7 +204,7 @@ AM_CONDITIONAL(ENABLE_GOOGLEBACKENDS, test "x$enable_google" = "xyes")
 if test "x$enable_google" = "xyes"; then
   i="System.Security"
   AC_MSG_CHECKING([for $i.dll (needed by Google backends)])
-  if test ! -e `$PKG_CONFIG --variable=prefix mono`/lib/mono/2.0/$i.dll; then
+  if test ! -e `$PKG_CONFIG --variable=prefix $mono_pc`/lib/mono/2.0/$i.dll; then
     AC_MSG_ERROR([missing required mono DLL: $i.dll])
   else
     AC_MSG_RESULT([found])
@@ -560,7 +569,7 @@ dnl I hope checking the presence of the qt-dotnet.dll should be enough
 if test "x$enable_qt" != "xno" -a "x$has_qyoto" = "xno"; then
 	i="qt-dotnet"
 	AC_MSG_CHECKING([for $i.dll])
-	if test ! -e `$PKG_CONFIG --variable=prefix mono`/lib/mono/2.0/$i.dll; then
+	if test ! -e `$PKG_CONFIG --variable=prefix $mono_pc`/lib/mono/2.0/$i.dll; then
 		AC_MSG_ERROR([missing required mono DLL: $i.dll])
 	else
 		AC_MSG_RESULT([found])

beagle/glue/mono-glue.c
@@ -79,18 +79,18 @@ memory_usage (MonoObject *obj, GHashTable *visited)
         type = mono_class_get_type (klass);
 
         /* This is an array, so drill down into it */
-        if (type->type == MONO_TYPE_SZARRAY)
+        if (mono_type_get_type (type) == MONO_TYPE_SZARRAY)
                 total += memory_usage_array ((MonoArray *) obj, visited);
 
         while ((field = mono_class_get_fields (klass, &iter)) != NULL) {
                 MonoType *ftype = mono_field_get_type (field);
                 gpointer value;
 
-                if ((ftype->attrs & (FIELD_ATTRIBUTE_STATIC | FIELD_ATTRIBUTE_HAS_FIELD_RVA)) != 0)
+                if ((mono_field_get_flags (field) & (FIELD_ATTRIBUTE_STATIC | FIELD_ATTRIBUTE_HAS_FIELD_RVA)) != 0)
                         continue;
 
                 /* FIXME: There are probably other types we need to drill down into */
-                switch (ftype->type) {
+                switch (mono_type_get_type (ftype)) {
 
                 case MONO_TYPE_CLASS:
                 case MONO_TYPE_OBJECT: